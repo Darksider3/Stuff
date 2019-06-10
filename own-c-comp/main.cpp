87da8bc25e23a6051576504faf4221823242fc6d
darksider3 <github@darksider3.de>
2019-06-10 15:26:11 +0200

    own C compiler new start; Scanner!

---
+++ own-c-comp/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <ctype.h>
+
+enum {
+  nullsym, intsym, tablesym, timesym, strsym, lparen, rparen, eofsym
+};
+
+class Scanner
+{
+public:
+  int ch;
+  FILE *f;
+  bool init(const char fn[])
+  {
+    if((f=fopen(fn, "r")) == NULL)
+    {
+      printf("Couldnt open file %s.", fn);
+      return false;
+    }
+    ch = ' ';
+    readch();
+
+  }
+
+  void readch()
+  {
+    ch = fgetc(f);
+    /*if(ch != EOF)
+      putchar((char)ch);
+    else
+      ch=EOF;*/
+  }
+
+  void loop_ch()
+  {
+    while(ch != EOF)
+    {
+      if(ch == '\n')
+        printf("ch: Newline\n");
+      else if(ch == ' ')
+        printf("ch: Space\n");
+      else
+        printf("ch: '%c'\n", ch);
+      readch();
+    }
+  }
+};
+
+int main() {
+  Scanner Test;
+  Test.init("../main.cpp");
+  Test.loop_ch();
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
