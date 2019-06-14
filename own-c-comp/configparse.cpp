dd19aa10cd006402f0c8deb1980e39ccc97865bc
darksider3 <github@darksider3.de>
2019-06-14 16:58:16 +0200

    Add filesize(), constructor initialiser list for filename

--- own-c-comp/configparse.cpp
+++ own-c-comp/configparse.cpp
@@ -11,13 +11,14 @@ private:
   std::string strLine;
   unsigned int CharInLine;
   unsigned int ProccessedLines;
+  std::string filename;
 public:
 
-  Configparse(std::string Fname="../test")
+  Configparse(std::string Fname="../test") : filename{Fname}
   {
     CharInLine=0;
     ProccessedLines=0;
-    f.open(Fname, std::ifstream::out);
+    f.open(filename, std::ifstream::out);
   }
   bool getLine()
   {
@@ -40,6 +41,24 @@ public:
     }
     else
       return strLine[CharInLine++];
+  }
+
+  unsigned long int filesize()
+  {
+    std::streampos fsize=0;
+    std::ifstream file(filename, std::ios::binary);
+    fsize=file.tellg();
+    file.seekg(0, std::ios::end);
+    fsize = file.tellg() - fsize;
+    file.close();
+    return fsize;
+  }
+  void parse()
+  {
+    int fsize = filesize();
+    getLine();
+
+
   }
 
   void test()
