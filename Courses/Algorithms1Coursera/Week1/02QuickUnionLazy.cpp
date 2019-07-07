d4bf5fc0975b878170da4cdf12246c1d73972ad5
darksider3 <github@darksider3.de>
2019-07-07 02:08:50 +0200

    forgot to change return 0 to N in root() function -.-

--- Courses/Algorithms1Coursera/Week1/02QuickUnionLazy.cpp
+++ Courses/Algorithms1Coursera/Week1/02QuickUnionLazy.cpp
@@ -9,28 +9,38 @@ private:
   size_t root(size_t N)
   {
     while(N != id[N]) N = id[N];  // chase parent pointers until reaching the root, means follow every number till we found the exact same. When id(N)==N, we found our root! :)
-    return 0;
+    return N;
   }
   
 public:
   QuickUnionLazy(size_t N)
   {
+    length = N;
     id = new int[N];
     for(size_t i = 0; i < N; i++) id[i] = i; // set up our array
   }
   
   bool connected(size_t p, size_t q)
   {
-    return(root(p) == root(q)); //check if the root is the same. Basically, if a root matches to different objects, it must be connected
+    return root(p) == root(q); //check if the root is the same. Basically, if a root matches to different objects, it must be connected
   }
   
   void Union(size_t p, size_t q)
   {
+      
     size_t i = root(p);
     size_t j = root(q);
-    id[i] = j; // Union the roots of p and q, means: Set the root of p to q's root, so p is a subtree of q now
+    std::cout << "root(p) = " << i << ", root(q) = " << j << std::endl;
+    id[j] = i; // Union the roots of p and q, means: Set the root of p to q's root, so p is a subtree of q now
   }
-  
+    
+  void dump()
+  {
+    for(size_t i = 0; i < length; i++)
+    {
+      std::cout << "id[" << i << "] = " << id[i] << std::endl;
+    }
+  }
   ~QuickUnionLazy()
   {
     delete[] id;
@@ -48,6 +58,7 @@ int main()
   test.Union(7, 8);
   test.Union(8, 9);
   test.Union(9, 10);
+    test.dump();
   if (test.connected(1, 2))
     std::cout <<  "1, 2 connected!" <<  std::endl;
   if (test.connected(2, 3))
@@ -59,7 +70,8 @@ int main()
   if (test.connected(8, 9))
     std::cout <<  "8, 9 connected!" <<  std::endl;
   if (test.connected(7, 10))
-    std::cout <<  "7, 10 connected!" <<  std::endl;
-  return 0;
+    std::cout <<  "7, 10 connected!" <<  std::endl;  
+  if (test.connected(15, 10))
+    std::cout <<  "15, 10 connected!" <<  std::endl;
   return 0;
 }
