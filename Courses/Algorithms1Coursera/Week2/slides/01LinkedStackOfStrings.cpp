11475ddf874705d375f56be502cb514b7c417031
darksider3 <github@darksider3.de>
2019-07-11 16:52:00 +0200

    new slide!

---
+++ Courses/Algorithms1Coursera/Week2/slides/01LinkedStackOfStrings.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+class LinkedStackOfStrings
+{
+protected:
+  class Node
+  {
+  public:
+    std::string item;
+    Node *next;
+  };
+  Node *first = nullptr;
+public:
+  bool isEmpty()
+  {
+    return first == nullptr;
+  }
+  
+  void push(std::string item)
+  {
+    Node *oldfirst = first;
+    first = new Node();
+    first->item = item;
+    first->next = oldfirst;
+  }
+  
+  std::string pop()
+  {
+    std::string item = first->item;
+    first = first->next;
+    return item;
+  }
+  
+  ~LinkedStackOfStrings()
+  {
+    struct Node *cur, *tmp;
+    cur = this->first;
+    while(cur != nullptr)
+    {
+      tmp = cur->next;
+      delete (cur);
+      cur = tmp;
+    }
+  }
+};
+
+int main()
+{
+  LinkedStackOfStrings stack;
+  std::string s = "hallo welt";
+  stack.push(s);
+  std::cout << stack.pop() << std::endl;
+}
