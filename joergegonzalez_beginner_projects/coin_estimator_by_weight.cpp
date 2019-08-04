df81986494f0e87679c4af9b44e27fbb4ea08f8d
darksider3 <github@darksider3.de>
2019-08-04 11:27:41 +0200

    Coin estimator by weight: Done

---
+++ joergegonzalez_beginner_projects/coin_estimator_by_weight.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath> //round
+
+struct Coin
+{
+  std::string name;
+  float weight = 2;
+  size_t wrapperSize = 50;
+};
+
+
+int estimateWrappers(struct Coin const c, size_t totalWeight)
+{
+  /* Pennies. 5g each, 50 per wrapper. Total 500g
+   * (500/5)=100/50=2
+   */
+  return (totalWeight / c.weight) / c.wrapperSize;
+}
+
+int main()
+{
+  std::string inputbuf;
+  while(true)
+  {
+    struct Coin CoinType;
+    size_t fullWeight;
+   
+    std::cout << "Which type you want an estimate from? Answer: ";
+    
+    std::cin >> inputbuf;
+    CoinType.name = inputbuf;
+    std::cin.clear();
+    inputbuf = "";
+    
+    std::cout << "Weight of a single Coin: ";
+    
+    std::cin >> inputbuf;
+    CoinType.weight = std::stoi(inputbuf);
+    std::cin.clear();
+    inputbuf = "";
+
+    std::cout << "Total weight of your coins: ";
+
+    std::cin >> inputbuf;
+    fullWeight = std::stoi(inputbuf);
+    std::cin.clear();
+    inputbuf = "";
+
+    std::cout << "\n\nYou could fill " << estimateWrappers(CoinType, fullWeight) << " wrappers with your " << CoinType.name << 
+      ", and got a total of " << round(fullWeight/CoinType.weight) << " Coins.\n\n";
+    
+    std::fflush(stdout);
+  }
+}
