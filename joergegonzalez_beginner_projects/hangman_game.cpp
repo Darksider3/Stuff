a2bd05edc1f7595a8ac5f0676db7bf5af27af586
darksider3 <github@darksider3.de>
2019-09-08 11:39:17 +0200

    stats todo, emplace back, explicit one-argument constructor, run=false->continue exit

--- joergegonzalez_beginner_projects/hangman_game.cpp
+++ joergegonzalez_beginner_projects/hangman_game.cpp
@@ -15,12 +15,13 @@ protected:
 public:
   struct Statistic
   {
+    //@TODO: Stats!
     size_t trys = 0;
     size_t wins = 0;
     size_t looses = 0;
   } STATS;
 
-  Hangman(std::vector<std::string> Words, int x = -1) : Dict{Words}
+  explicit Hangman(std::vector<std::string> Words, int x = -1) : Dict{Words}
   {
     if(x == -1)
     {
@@ -89,14 +90,14 @@ public:
 int main()
 {
   std::vector<std::string> Words;
-  Words.push_back("Hangman");
-  Words.push_back("Oversee");
-  Words.push_back("Towel");
-  Words.push_back("Watchtover");
-  Words.push_back("Insertion");
-  Words.push_back("Ladybird");
-  Words.push_back("Test");
-  Words.push_back("Halleluja!");
+  Words.emplace_back("Hangman");
+  Words.emplace_back(("Oversee");
+  Words.emplace_back(("Towel");
+  Words.emplace_back(("Watchtover");
+  Words.emplace_back(("Insertion");
+  Words.emplace_back(("Ladybird");
+  Words.emplace_back(("Test");
+  Words.emplace_back(("Halleluja!");
   Hangman Game(Words);
   /*std::cout << Game.getGameString() << std::endl;
   if(Game.FindLetter('e'))
@@ -115,6 +116,11 @@ int main()
       std::cout << "Current: \n" << Game.getGameString() << "\n";
       std::cout << "Your guess: ";
       Guess = getchar();
+      if(Guess == '^')
+      {
+        run=false;
+        continue;
+      }
       std::cout << "\n";
       if(Game.FindLetter(Guess))
       {
