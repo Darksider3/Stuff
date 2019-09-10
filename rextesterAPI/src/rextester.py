3c54015bb909a2a54a6ed94a93182d41ade45c29
darksider3 <github@darksider3.de>
2019-09-10 18:05:58 +0200

    putCode sceleton+some error correction in runCode

--- rextesterAPI/src/rextester.py
+++ rextesterAPI/src/rextester.py
@@ -40,8 +40,9 @@ def runCode(code: str, lang: int) -> dict:
     H["X-Requested-With"] = "XMLHttpRequest"
     H["Origin"] = "https://rextester.com"
     import urllib
-    data = {"LanguageChoiceWrapper": 27, "EditorChoiceWrapper":1, "LayoutChoiceWrapper":1, "Program": code, 
-            "CompilerArgs": "-Wall -std=c++14 -stdlib=libc++ -O2 -o a.out source_file.cpp"}
+    data = {"LanguageChoiceWrapper": lang, "EditorChoiceWrapper":1, "LayoutChoiceWrapper":1, "Program": code, 
+            "CompilerArgs": "-Wall -std=c++14 -stdlib=libc++ -O2 -o a.out source_file.cpp", "ShowWarning": "false", "Privacy": "", "PrivacyUsers": "", "Title": "",
+            "SavedOutput": "", "WholeError": "", "WholeWarning": "", "StatsToSave": "", "CodeGuid": "", "IsInEditMode": "False", "IsLive": "False"}
     data = urllib.parse.urlencode(data)
 
     try:
@@ -54,9 +55,36 @@ def runCode(code: str, lang: int) -> dict:
     else:
         return response.text;
     return
-    
+   
+#curl 'https://rextester.com/rundotnet/save' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0' -H 'Accept: text/plain, */*; q=0.01' -H 'Accept-Language: de' --compressed -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: https://rextester.com' -H 'Connection: keep-alive' -H 'Referer: https://rextester.com/l/cpp_online_compiler_clang' --data 'LanguageChoiceWrapper=27&EditorChoiceWrapper=1&LayoutChoiceWrapper=1&Program=%23include+%3Ciostream%3E%0D%0Aint+main()%0D%0A%7B%0D%0A++++std%3A%3Acout+%3C%3C+%22Hello%2C+world!%5Cn%22%3B%0D%0A%7D&CompilerArgs=-Wall+-std%3Dc%2B%2B14+-stdlib%3Dlibc%2B%2B+-O2+-o+a.out+source_file.cpp&Input=&ShowWarnings=false&Privacy=&PrivacyUsers=&Title=Save&SavedOutput=Hello%2C+world!%0D%0A&WholeError=&WholeWarning=&StatsToSave=Compilation+time%3A+0.63+sec%2C+absolute+running+time%3A+0.07+sec%2C+cpu+time%3A+0.01+sec%2C+memory+peak%3A+3+Mb%2C+absolute+service+time%3A+0%2C71+sec&CodeGuid=&IsInEditMode=False&IsLive=False'
 # uploads code and returns the new url
-def putCode(code: str) -> str:
+def putCode(code: str, lang: int, name: str) -> str:
+    url = f'{Base}/rundotnet/save'
+    H = Headers
+    H["Content-Type"] = "application/x-ww-form-urlencoded"
+    H["X-Requested-With"] = "XMLHttpRequest"
+    H["Origin"] = "https://rextester.com"
+    import urllib, json
+
+    RunRequest = json.loads(runCode(code, lang))
+    #{"Warnings":null,"Errors":null,"Result":"Hello, world!\n","Stats":"Compilation time: 0.83 sec, absolute running time: 0.07 sec, cpu time: 0.01 sec, memory peak: 3 Mb, absolute service time: 0,91 sec","Files":null}
+    SavedOutput = RunRequest["Result"]
+
+    data = {"LanguageChoiceWrapper": lang, "EditorChoiceWrapper":1, "LayoutChoiceWrapper":1, "Program": code, 
+            "CompilerArgs": "-Wall -std=c++14 -stdlib=libc++ -O2 -o a.out source_file.cpp", "ShowWarning": "false", 
+            "Privacy": "", "PrivacyUsers": "", "Title": name,
+            "SavedOutput": SavedOutput, "WholeError": "", "WholeWarning": "", "StatsToSave": "", "CodeGuid": "", 
+            "IsInEditMode": "False", "IsLive": "False"}
+    data = urllib.parse.urlencode(data)
+    # @TODO: Perform request, parse Link out of response and deliver \o/
+    try:
+        pass
+    except requests.HTTPError as err:
+        pass
+    except Exception as err:
+        pass
+    else:
+        pass
     return;
 
 def _unidiff_output(expected, actual):
