4344103ccf74e05c7ac0b3c5a299b4fede7e92d8
darksider3 <github@darksider3.de>
2019-09-10 17:47:28 +0200

    runCode implemented

--- rextesterAPI/src/rextester.py
+++ rextesterAPI/src/rextester.py
@@ -3,13 +3,9 @@ import sys, html
 from bs4 import BeautifulSoup
 
 #curl 'https://rextester.com/TSBV14831' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: de' --compressed -H 'Referer: https://rextester.com/l/cpp_online_compiler_clang' -H 'Connection: keep-alive' -H 'Cookie: ASP.NET_SessionId=t4iaj3ycanhikkvb5aepiuq2' -H 'Upgrade-Insecure-Requests: 1' -H 'Cache-Control: max-age=0'
-
 Headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:7    0.0) Gecko/20100101 Firefox/70.0",
-        "Accept": "text/html,application/xhtml+xml,application/xm    l;q=0.9,*/*;q=0.8",
-        "Referer": "https://rextester.com/l/cpp_online_compiler_clang",}
-
+        "Referer": "https://rextester.com/l/cpp_online_compiler_clang", "charset": "UTF-8"}
 Base = "https://rextester.com/"
-
 Session = requests.Session()
 
 def getExistingCode(url: str, withbase=False) -> str:
@@ -36,7 +32,29 @@ def getExistingCode(url: str, withbase=False) -> str:
     #  <textarea class="editor" spellcheck="false" cols="1000" id="Program" name="Program" rows="30" style="width: 100%;resize:none;">$CODE</textarea>
     return ret
 
+#curl 'https://rextester.com/rundotnet/Run' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0' -H 'Accept: text/plain, */*; q=0.01' -H 'Accept-Language: de' --compressed -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: https://rextester.com' -H 'Connection: keep-alive' -H 'Referer: https://rextester.com/l/cpp_online_compiler_clang' --data 'LanguageChoiceWrapper=27&EditorChoiceWrapper=1&LayoutChoiceWrapper=1&Program=%23include+%3Ciostream%3E%0D%0Aint+main()%0D%0A%7B%0D%0A++++std%3A%3Acout+%3C%3C+%22Hello%2C+world!%5Cn%22%3B%0D%0A%7D&CompilerArgs=-Wall+-std%3Dc%2B%2B14+-stdlib%3Dlibc%2B%2B+-O2+-o+a.out+source_file.cpp&Input=&ShowWarnings=false&Privacy=&PrivacyUsers=&Title=&SavedOutput=&WholeError=&WholeWarning=&StatsToSave=&CodeGuid=&IsInEditMode=False&IsLive=False'
+def runCode(code: str, lang: int) -> dict:
+    url = f'{Base}rundotnet/Run'
+    H = Headers
+    H["Content-Type"] = "application/x-www-form-urlencoded"
+    H["X-Requested-With"] = "XMLHttpRequest"
+    H["Origin"] = "https://rextester.com"
+    import urllib
+    data = {"LanguageChoiceWrapper": 27, "EditorChoiceWrapper":1, "LayoutChoiceWrapper":1, "Program": code, 
+            "CompilerArgs": "-Wall -std=c++14 -stdlib=libc++ -O2 -o a.out source_file.cpp"}
+    data = urllib.parse.urlencode(data)
 
+    try:
+        response = Session.post(url, data=data, headers=Headers)
+        response.raise_for_status()
+    except requests.HTTPError as err:
+        print(f"Couldn't let that code run! :/ HTTP-Error: {err}")
+    except Exception as err:
+        print(f"Uncatchable exception occured: {err}")
+    else:
+        return response.text;
+    return
+    
 # uploads code and returns the new url
 def putCode(code: str) -> str:
     return;
@@ -45,9 +63,7 @@ def _unidiff_output(expected, actual):
     """
     Helper function. Returns a string containing the unified diff of two multiline strings.
     """
-
     import difflib
-    
     #expected = [line for line in expected.split('\n')]
     #actual = [line for line in actual.split('\n')]
     #diff=difflib.unified_diff(expected, actual)
@@ -60,3 +76,11 @@ Exist= getExistingCode("https://rextester.com/TSBV14831")
 print(Exist)
 from pprint import pprint
 sys.stdout.writelines(_unidiff_output('std::cout << "Hello, world!\n";', Exist))
+
+TestCode = """#include <iostream>
+int main()
+{
+    std::cout << "Hello, world!\\n";
+}
+"""
+print(runCode(TestCode, 27))
