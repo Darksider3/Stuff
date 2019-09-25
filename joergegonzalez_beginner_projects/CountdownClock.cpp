63e6c4705c539d231b886af4f409e336c535420f
darksider3 <github@darksider3.de>
2019-09-25 21:29:02 +0200

    Counter and alarm implemented, yay. Missing stuff: Help and exit. :>

--- joergegonzalez_beginner_projects/CountdownClock.cpp
+++ joergegonzalez_beginner_projects/CountdownClock.cpp
@@ -9,7 +9,7 @@
 //#define WINDOW_HEIGHT 600
 bool quit = false;
 
-void playSoundOnce(std::string soundFile)
+void playSoundOnce(std::string soundFile, std::chrono::duration<float> T=std::chrono::milliseconds(100))
 {
   int audio_rate = 22050;
   Uint16 audio_format = AUDIO_S16SYS;
@@ -35,13 +35,15 @@ void playSoundOnce(std::string soundFile)
     fprintf(stderr, "Unable to play WAV file: %s\n", Mix_GetError()); 
   } 
   while(Mix_Playing(channel) != 0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  std::this_thread::sleep_for(T);
   Mix_FreeChunk(sound);
   Mix_CloseAudio(); 
 }
 
 int EventFilterTest(void *userdata, SDL_Event *event)
 {
+  if(!userdata)
+    return 0;
   if(event->type == SDL_KEYDOWN)
   {
     quit = true;
@@ -60,6 +62,22 @@ int EventFilterTest(void *userdata, SDL_Event *event)
 
 int main(int argc, char ** argv) {
   /* Initialize SDL */
+  //std::chrono::duration<float> Timer;
+  std::chrono::duration<double> Timer = std::chrono::milliseconds(1000);
+  if(argc > 1)
+  {
+    int temp = std::stoi(argv[2]);
+    std::string tmpStr = std::string(argv[1]);
+    if(tmpStr == "ms")
+      Timer = std::chrono::milliseconds(temp);
+    else if (tmpStr == "s")
+      Timer = std::chrono::seconds(temp);
+    else if (tmpStr == "m")
+      Timer = std::chrono::minutes(temp);
+    else if (tmpStr == "h")
+      Timer = std::chrono::hours(temp);
+
+  }
   SDL_Init(SDL_INIT_EVERYTHING);
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) 
   { 
@@ -67,16 +85,17 @@ int main(int argc, char ** argv) {
     return 1; 
   } 
   SDL_Event event;
+  int counter= Timer.count();
   while(quit == false )
   {
-    SDL_WaitEvent(&event);
-    if(event.type == SDL_QUIT)
-      break;
-    if(event.type == SDL_KEYDOWN)
+    if(counter!=0)
     {
-      std::cout << event.key.keysym.sym << "\n";
+      std::cout << '\b' << '\r' << "\033[K" << "Time remaining: " << counter--;
+      std::cout.flush();
+      std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-    playSoundOnce("sound.wav");
+    else 
+      playSoundOnce("sound.wav");
   }
   /* Deinitialize everything */
   SDL_Quit();
