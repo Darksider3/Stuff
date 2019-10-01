b93bf06f79cce044faf9efc3084fb8bfc88c6301
darksider3 <github@darksider3.de>
2019-10-01 14:47:09 +0200

    simple https! \o/

--- joergegonzalez_beginner_projects/net/simplehttpsget.cpp
+++ joergegonzalez_beginner_projects/net/simplehttpsget.cpp
@@ -1,15 +1,76 @@
 #include <iostream>
 #include <curl/curl.h>
 
-int main()
+
+static std::string buffer;
+static char errorBuffer[CURL_ERROR_SIZE];
+
+static int writer(char *data, size_t size, size_t nmemb, std::string *writerData)
+{
+  if(writerData == NULL)
+    return 0;
+
+  writerData->append(data, size*nmemb);
+
+  return size*nmemb;
+}
+
+static bool init(CURL *&conn, char *url)
+{
+  CURLcode code;
+ 
+  conn = curl_easy_init();
+ 
+  if(conn == NULL) {
+    fprintf(stderr, "Failed to create CURL connection\n");
+    exit(EXIT_FAILURE);
+  }
+ 
+  code = curl_easy_setopt(conn, CURLOPT_ERRORBUFFER, errorBuffer);
+  if(code != CURLE_OK) {
+    fprintf(stderr, "Failed to set error buffer [%d]\n", code);
+    return false;
+  }
+ 
+  code = curl_easy_setopt(conn, CURLOPT_URL, url);
+  if(code != CURLE_OK) {
+    fprintf(stderr, "Failed to set URL [%s]\n", errorBuffer);
+    return false;
+  }
+ 
+  code = curl_easy_setopt(conn, CURLOPT_FOLLOWLOCATION, 1L);
+  if(code != CURLE_OK) {
+    fprintf(stderr, "Failed to set redirect option [%s]\n", errorBuffer);
+    return false;
+  }
+ 
+  code = curl_easy_setopt(conn, CURLOPT_WRITEFUNCTION, writer);
+  if(code != CURLE_OK) {
+    fprintf(stderr, "Failed to set writer [%s]\n", errorBuffer);
+    return false;
+  }
+ 
+  code = curl_easy_setopt(conn, CURLOPT_WRITEDATA, &buffer);
+  if(code != CURLE_OK) {
+    fprintf(stderr, "Failed to set write data [%s]\n", errorBuffer);
+    return false;
+  }
+ 
+  return true;
+}
+
+int main(int argc, char *argv[])
 {
   CURL *curl;
-  int err;
   std::string url = "https://darksider3.de";
-
-  curl = curl_easy_init();
-  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-
-
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+  if(!init(curl, argv[1]))
+  {
+    fprintf(stderr, "Sorry, failure.. %s \n", errorBuffer);
+    exit(EXIT_FAILURE);
+  }
+  curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+  std::cout << buffer;
 }
 
