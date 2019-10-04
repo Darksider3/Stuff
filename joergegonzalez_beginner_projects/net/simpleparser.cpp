b020e0533102198ee27da4bb20deeffbabd9b47e
darksider3 <github@darksider3.de>
2019-10-04 12:30:22 +0200

    welp

--- joergegonzalez_beginner_projects/net/simpleparser.cpp
+++ joergegonzalez_beginner_projects/net/simpleparser.cpp
@@ -1,45 +1,74 @@
 #include <iostream>
-#define SEPERATOR ','
-
+#include <cstdio>
+#include <cerrno>
 /*
- ******************************IS_* functions
+ * HELPER
  */
-bool isDigit();
-bool isFloating_point_digit();
-bool isSeperator();
-bool isBracket();
-bool isEscape();
-bool isQuote();
-
-/*
- ******************************IN_* functions
- */
-
-bool inQuote();
-bool inString();
-bool inNum(); //either integer-digit or floating-digit!
-
-/*
- *******************************SPECIAL
- */
-
-int scopeLevel();
-bool isSomeScopeDangling();
-
-
-char next();
-void openFile();
-
-
-
-bool is_seperator(char ch)
+std::string get_file_contents(const char *filename)
 {
-  switch(ch)
+  std::FILE *fp = std::fopen(filename, "rb");
+  if (fp)
   {
-    case ',':
-    case '=':
-    case ';':
-      return true;
+    std::string contents;
+    std::fseek(fp, 0, SEEK_END);
+    contents.resize(std::ftell(fp));
+    std::rewind(fp);
+    std::fread(&contents[0], 1, contents.size(), fp);
+    std::fclose(fp);
+    return(contents);
   }
-  return false;
+  throw(errno);
 }
+
+enum JSON_TYPE {
+  TYPE_NULL,
+  TYPE_BOOL,
+  TYPE_NUMBER,
+  TYPE_OBJECT,
+  TYPE_ARRAY,
+  TYPE_STRING,
+  TYPE_KEY
+};
+
+struct json_val
+{
+  int T;
+  union
+  {
+    bool boolean;
+    double number;
+    std::string str;
+    std::string key;
+    //std::vector array;
+    //std::vector object;
+  } value;
+};
+
+class Tokenizer
+{
+private:
+  std::string File;
+  size_t Position;
+public: 
+  Tokenizer(std::string &F)
+  {
+    File=get_file_contents(F.c_str());
+    Position = 0;
+  }
+  char next()
+  {
+    if(Position > File.size())
+      return -1;
+
+    return File[Position++];
+  }
+  bool isSeperator(); // Return if Char is a seperator, for json that would be ,(comma)
+  bool isChar(const char &c, unsigned int pos);
+    //return c == CURRENT_POS_CHAR;
+  bool ignoreWhitespace();
+    /*
+    while(**cursor == '\t' || **cursor  == '\r' || **cursor == '\n' || **cursor == ' ') ++(*cursor);
+    */
+    // work on cursor, ignore everything that's a space or newline, exit on nullbyte.
+    // return true until nullbyte
+};
