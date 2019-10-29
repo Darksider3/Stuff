1501295757afe9faf6e490309f2f748324e8df64
Darksider3 <github@darksider3.de>
2019-10-29 12:07:00 +0100

    V2 of that api. Still working on it

---
+++ net/jsonparser/simpleparserv2.cpp
@@ -0,0 +1,113 @@
+#include "simpleparserv2.h"
+
+simpleparserv2::simpleparserv2(const std::string& t) : tok{t}
+{}
+
+void simpleparserv2::parse()
+{
+  auto emit = [&](auto Type,  JSON_Object::Value::Typename T = JSON_Object::Value::Typename::None)
+  {
+    JSON_Object tmp;
+    tmp.value.Type = T;
+    tmp.Sym = Type;
+    tmp.Pos = static_cast<int>(tok.getPos());
+    H.emplace_back(tmp);
+  };
+  while(!tok.eof())
+  {
+    char cur = tok.next();
+    if(cur == '\0')
+    {
+      emit(JSON_SYM::end_of_input);
+      break;
+    }
+    if(cur == '{')
+    {
+      emit(JSON_SYM::begin_object);
+      continue;
+    }
+    if(cur == '}')
+    {
+      emit(JSON_SYM::end_object);
+      continue;
+    }
+    if(cur == '[')
+    {
+      emit(JSON_SYM::begin_array);
+      continue;
+    }
+    if(cur == ']')
+    {
+      emit(JSON_SYM::end_array);
+      continue;
+    }
+    if(cur == ',')
+    {
+      emit(JSON_SYM::member_seperator);
+      continue;
+    }
+    if(cur == ':')
+    {
+      emit(JSON_SYM::value_separator);
+      continue;
+    }
+    if(cur == 'f' && tok.peek("alse"))
+    {
+      emit(JSON_SYM::literal_false, JSON_Object::Value::Typename::Boolean);
+      tok.setPos(tok.getPos()+4);
+      continue;
+    }
+    if(cur == 't' && tok.peek("rue"))
+    {
+      emit(JSON_SYM::literal_true, JSON_Object::Value::Typename::Boolean);
+      tok.setPos(tok.getPos()+3);
+      continue;
+    }
+    if(cur == 'n' && tok.peek("ull"))
+    {
+      emit(JSON_SYM::literal_null, JSON_Object::Value::Typename::Null);
+      tok.setPos(tok.getPos()+3);
+      continue;
+    }
+    if(cur == '"' || cur == '\'')
+    {
+      JSON_Object found_str;
+      StringResult tmp = tok.getStrOnPos(tok.getPos()-1);
+      found_str.Pos = static_cast<int>(tok.getPos()-1);
+      found_str.Sym = JSON_SYM::value_string;
+      found_str.value.String = tmp;
+      found_str.value.Type = JSON_Object::Value::Typename::String;
+      H.emplace_back(found_str);
+      tok.setPos(tok.getPos()+tmp.str.size()+1); // we didnt include the quotes - so size is already +1 off, but we need +1 for the second one
+      continue;
+    }
+    if(std::isdigit(cur))
+    {
+      struct NumberResult num;
+      struct JSON_Object found_num;
+      num = tok.getNumber();
+      assert(!num.str.empty() && "How is this empty?");
+      found_num.value.Type = JSON_Object::Value::Typename::Number;
+      if(num.hasDot)
+      {
+        found_num.Sym = JSON_SYM::value_float;
+      }
+      else
+      {
+        found_num.Sym = JSON_SYM::value_number;
+      }
+      found_num.value.number = num;
+      H.emplace_back(found_num);
+      tok.setPos(tok.getPos()+num.str.size()-1); // -1 here because size() is not null-indexed, but numbers don't start with an own identifier...
+      continue;
+    }
+    std::cout << "UNKNOWN CHAR : " << cur << "\n";
+  }
+  return;
+}
+
+int main()
+{
+  simpleparserv2 Test{"./example.json"};
+  Test.parse();
+}
---
+++ net/jsonparser/simpleparserv2.h
@@ -0,0 +1,303 @@
+#ifndef SIMPLEPARSERV2_H
+#define SIMPLEPARSERV2_H
+
+#include <iostream>
+#include <cstdio>
+#include <cerrno>
+#include <vector>
+#include <cstring>
+#include <cassert>
+//#include <zconf.h>
+#define FOR_EACH_SYM \
+  SYMBOLNAME (Unknown)          \
+  SYMBOLNAME (literal_true)     \
+  SYMBOLNAME (literal_false)    \
+  SYMBOLNAME (literal_null)     \
+  SYMBOLNAME (value_string)     \
+  SYMBOLNAME (value_number)     \
+  SYMBOLNAME (value_float)      \
+  SYMBOLNAME (begin_array)      \
+  SYMBOLNAME (begin_object)     \
+  SYMBOLNAME (end_array)        \
+  SYMBOLNAME (end_object)       \
+  SYMBOLNAME (value_separator)  \
+  SYMBOLNAME (member_seperator) \
+  SYMBOLNAME (parse_error)      \
+  SYMBOLNAME (end_of_input)
+std::string get_file_contents(const char *filename)
+{
+  std::FILE *fp = std::fopen(filename, "rb");
+  if (fp)
+  {
+    std::string contents;
+    std::fseek(fp, 0, SEEK_END);
+    contents.resize(std::ftell(fp));
+    std::rewind(fp);
+    std::fread(&contents[0], 1, contents.size(), fp);
+    std::fclose(fp);
+    return(contents);
+  }
+  throw(errno);
+}
+
+enum class JSON_SYM {
+  #define SYMBOLNAME(x) x,
+  FOR_EACH_SYM
+  #undef SYMBOLNAME
+};
+struct NumberResult
+{
+  bool hasDot = {false};
+  bool hasE = {false};
+  long StartPos = {-1}, EndPos = {-1};
+  double Number= {0};
+  std::string str = {""};
+};
+
+
+struct StringResult
+{
+  std::string str;
+  long StartPos={-1}, EndPos={-1};
+};
+
+struct JSON_Object
+{
+  std::vector<JSON_Object> ArryObj;
+  struct Value
+  {
+    StringResult String;
+    NumberResult number = {};
+    bool literal;
+    enum class Typename
+    {
+      Number,
+      String,
+      Boolean,
+      Null,
+      None
+    };
+    Typename Type;
+  } value;
+  JSON_SYM Sym;
+
+  int Pos;
+};
+
+class Tokenizer
+{
+private:
+  std::string File;
+  size_t Position;
+
+public:
+  explicit Tokenizer(const std::string &F)
+  {
+    try
+    {
+      File = get_file_contents(F.c_str());
+      Position = 0;
+    }
+    catch(int &e)
+    {
+      std::cout << "Sorry, something went wrong here! Your file doesn't exist.\n";
+      std::exit(e);
+    }
+  }
+
+  size_t size() const
+  {
+    return File.size();
+  }
+
+  //@TODO Bounds checking File.size();
+  //@TODO Also exception
+  void setPos(size_t const pos)
+  {
+    Position = pos;
+  }
+
+  size_t getPos() const
+  {
+    return Position;
+  }
+
+  bool ignoreWhitespace()
+  {
+    while(File[Position] == '\t' || File[Position]  == '\r' || File[Position] == '\n' || File[Position] == ' ')
+    {
+      if(Position > File.size())
+        return false;
+      ++Position;
+    }
+    return true;
+  }
+
+  char next()
+  {
+    if(!ignoreWhitespace())
+      return 0;
+    return File[Position++];
+  }
+
+  bool eof() const
+  {
+    return File.size() <= Position;
+  }
+
+  // @TODO: Exception. Now.
+  struct NumberResult getNumber(size_t FromPos = 0)
+  {
+    if(FromPos == 0)
+      FromPos = this->getPos()-1;
+
+    assert(FromPos <= File.size() && "First argument is bigger than the file size!");
+
+    size_t cursor=FromPos;
+    NumberResult res;
+    res.hasDot = false;
+    bool runner=true;
+    std::string temp;
+    for(;runner; ++cursor)
+    {
+
+      if(std::isdigit(File[cursor]))
+      {
+        temp+=File[cursor];
+        continue;
+      }
+      else if(File[cursor] == '.' && std::isdigit(File[cursor+1]))
+      {
+        res.hasDot = true;
+        temp+=File[cursor];
+        continue;
+      }
+      else if(res.hasE == false && (File[cursor] == 'e' || File[cursor] == 'E'))
+      {
+        res.hasE = true;
+        temp+=File[cursor];
+        continue;
+      }
+      else if(res.hasE == true && (File[cursor] == 'e' || File[cursor] == 'E'))
+      {
+        assert(false && "multiple exponents");
+      }
+      else if(File[cursor] == '-' || File[cursor] == '+')
+      {
+        temp+=File[cursor];
+        continue;
+      }
+      else
+        break;
+    }
+    res.StartPos = static_cast<long>(FromPos);
+    res.EndPos = static_cast<long>(--cursor);
+    res.str = temp;
+    return res;
+  }
+
+  StringResult getStrOnPos(size_t startingPos = 0)
+  {
+    auto isQuote = [](char ch)
+    {
+      return (ch == '\'' || ch == '"');
+    };
+    auto isViableHexChar=[](char s)
+    {
+      return (std::isdigit(s) ||
+         s == 'A' || s == 'a' ||
+         s == 'B' || s == 'b' ||
+         s == 'C' || s == 'c' ||
+         s == 'D' || s == 'd' ||
+         s == 'E' || s == 'e');
+    };
+    if(startingPos == 0)
+    {
+      startingPos = getPos();
+    }
+    assert(startingPos <= File.size() && "Requested startingPosition is bigger then the actual file size.");
+    StringResult returner;
+    bool escaped = false;
+    std::string temp;
+    char StrStarter = peek(startingPos);
+    char cur;
+    if(!isQuote(StrStarter))
+    {
+      assert(false && "Current character isn't a quote. Valid are ' and \".");
+    }
+    // looks like a string?
+
+    bool runner = true;
+    unsigned long i = startingPos+1;
+    for(; runner && !eof(); ++i)
+    {
+      cur = peek(static_cast<size_t>(i));
+      if(cur == '\\')
+      {
+        if((peek(static_cast<size_t>(i+1)) == 'u' || peek(static_cast<size_t>(i+1)) == 'U') &&
+           isViableHexChar(peek(static_cast<size_t>(i+1))) &&
+           isViableHexChar(peek(static_cast<size_t>(i+2))) &&
+           isViableHexChar(peek(static_cast<size_t>(i+3))) &&
+           isViableHexChar(peek(static_cast<size_t>(i+4)))) // @TODO OTHERWISE escaped == True bla
+        {
+          // @TODO it's a valid codepoint. Process it...
+        }
+        temp+=cur;
+        escaped=true;
+      }
+      else if (cur == StrStarter)
+      {
+        //str end!
+        if(!escaped)
+          runner=false;
+        else
+        {
+          temp+=cur;
+          escaped=false;
+        }
+      }
+      else
+      {
+        temp+=cur;
+      }
+    }
+    returner.str = temp;
+    returner.StartPos = static_cast<long>(startingPos);
+    returner.EndPos = static_cast<long>(i);
+    return returner;
+  }
+
+  char peek(size_t Pos=0) const
+  {
+    if(Pos == 0)
+      Pos = this->getPos();
+    assert(Pos <= File.size() && "delivered position is actually bigger then the file size!");
+
+    return File[Pos];
+  }
+  bool peek(const std::string &strToPeekFor) const
+  {
+    bool peeker = true;
+    for(size_t i = 0; i != strToPeekFor.size(); ++i)
+    {
+      if(strToPeekFor[i] != peek(getPos()+i))
+      {
+        peeker = false;
+        break;
+      }
+    }
+    return peeker;
+  }
+};
+
+class simpleparserv2
+{
+public:
+  Tokenizer tok;
+  std::vector<JSON_Object> H;
+
+  simpleparserv2(const std::string&);
+  void parse();
+};
+
+#endif // SIMPLEPARSERV2_H
