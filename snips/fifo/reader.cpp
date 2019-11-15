a40ae03a847067c69fe3ca7df9dfeba8293c4338
Darksider3 <github@darksider3.de>
2019-11-15 03:10:37 +0100

    A little bit fun with FIFOs
    Works pretty nice, but not sure how to catch up to events that are
    already in the queue... o.o

---
+++ snips/fifo/reader.cpp
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <sys/select.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <iostream>
+#include <filesystem>
+#include <cstring>
+
+int main()
+{
+  int fd;
+  int selector;
+  fd_set reader;
+  char buf[1024];
+    for(;;)
+    {
+      if(std::filesystem::exists("./fifo"))
+      {
+        break;
+      }
+      else
+      {
+        std::cout << "wait for fifo\n";
+        sleep(1);
+      }
+    }
+  fd = open("./fifo", O_RDWR | O_NONBLOCK);
+  if(fd == -1 )
+  {
+    std::cout << "nope";
+  }
+  FD_ZERO(&reader);
+  FD_SET(fd, &reader);
+  for(;;)
+  {
+    selector = select(fd+1, &reader, 0, 0, 0);
+    if(!selector)
+      continue;
+    if(selector == -1)
+    {
+      perror("select error");
+      return EXIT_FAILURE;
+    }
+    if(FD_ISSET(fd, &reader))
+    {
+      std::cout << "Ready state! \n";
+      read(fd, buf, sizeof(buf));
+      if(strcmp(buf, "end") == 0)
+      {
+        std::cout << "End!\n";
+        return EXIT_SUCCESS;
+      }
+      std::cout << buf << "\n";
+    }
+  }
+}
