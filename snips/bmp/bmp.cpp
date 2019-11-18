69a1c96227b9eec13349b791d8be3152bbb7302a
Darksider3 <github@darksider3.de>
2019-11-18 15:25:05 +0100

    Resizing! SCROLLING! Quit with Q! Reset Pic with ESC!Resizing! SCROLLING! Quit with Q! Reset Pic with ESC!

--- snips/bmp/bmp.cpp
+++ snips/bmp/bmp.cpp
@@ -4,6 +4,7 @@
 #include <typeinfo>
 #include <SFML/Graphics.hpp>
 
+#define SCROLLFACTOR 4
 
 bmp::Impl::Impl(std::string &FN)
 {
@@ -128,30 +129,48 @@ int main()
   auto data = T.legacyUint8RGBA();
   unsigned int widthpx=static_cast<unsigned int>(T.dib.width_px);
   unsigned int heightpx=static_cast<unsigned int>(T.dib.height_px);
-  sf::RenderWindow window(sf::VideoMode(128*5, 128*5), "BMP View");
-  sf::RectangleShape shape(sf::Vector2(static_cast<float>(widthpx)*5, static_cast<float>(heightpx)*5));
+  sf::RenderWindow window(sf::VideoMode(520, 520), "BMP View");
+  sf::RectangleShape shape(sf::Vector2(static_cast<float>(window.getSize().x)/2, static_cast<float>(window.getSize().y)/2));
   sf::Image img;
   DBG << widthpx << "x" << heightpx <<":"<<T.DATA.size();
   img.create(widthpx, heightpx, data);
   DBG << img.getSize().x << "x" << img.getSize().y;
   img.flipVertically();
-  img.saveToFile("./TESTER.bmp");
   sf::Texture texture;
   texture.loadFromImage(img);
   DBG << "Texture: " << texture.getSize().x << "x" << texture.getSize().y;
   shape.setTexture(&texture);
+  shape.setPosition((window.getSize().x - shape.getSize().x)/2, (window.getSize().y - shape.getSize().y)/2);
   while (window.isOpen())
   {
-      sf::Event event;
-      while (window.pollEvent(event))
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+      if (event.type == sf::Event::MouseWheelScrolled)
       {
-          if (event.type == sf::Event::Closed)
-              window.close();
+        shape.setSize(sf::Vector2(shape.getSize().x+event.mouseWheelScroll.delta*SCROLLFACTOR, shape.getSize().y+event.mouseWheelScroll.delta*SCROLLFACTOR));
       }
-
-      window.clear();
-      window.draw(shape);
-      window.display();
+      if(event.type == sf::Event::KeyPressed)
+      {
+        switch(event.key.code)
+        {
+          case sf::Keyboard::Escape:
+            shape.setSize(sf::Vector2(static_cast<float>(window.getSize().x)/2, static_cast<float>(window.getSize().y)/2));
+            break;
+          case sf::Keyboard::Q:
+            return EXIT_SUCCESS;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    shape.setPosition((window.getSize().x - shape.getSize().x)/2, (window.getSize().y - shape.getSize().y)/2);
+    window.clear();
+    window.draw(shape);
+    window.display();
   }
   delete[] data;
 }
