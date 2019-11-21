c6ba84de1c641d118a6fc977a8ea801e7a62526f
Darksider3 <github@darksider3.de>
2019-11-21 11:35:29 +0100

    Resizing works! FINALLY! NO OUT OF BOUNDS ANYMOOOOOORE
    SFMLs Views doesn't get updated on the fly on window resizes, so i had
    to set a new view an rearrange the shape in it. Works like a charm now!
    \o/
    
    Note: The sf::View is clunky AF.

--- snips/bmp/main.cpp
+++ snips/bmp/main.cpp
@@ -17,8 +17,10 @@ int main()
   auto data = T.legacyUint8RGBA();
   unsigned int widthpx=static_cast<unsigned int>(T.dib.width_px);
   unsigned int heightpx=static_cast<unsigned int>(T.dib.height_px);
-  sf::RenderWindow window(sf::VideoMode(520, 520), "BMP View");
-  sf::RectangleShape shape(sf::Vector2f(static_cast<float>(window.getSize().x)/2, static_cast<float>(window.getSize().y)/2));
+  sf::Vector2u static Windowpx=sf::Vector2u(520, 520);;
+  sf::RenderWindow window(sf::VideoMode(Windowpx.x, Windowpx.y), "BMP View");
+  sf::Vector2f Oldshape = sf::Vector2f(static_cast<float>(window.getSize().x)/2, static_cast<float>(window.getSize().y)/2);
+  sf::RectangleShape shape(Oldshape);
   sf::Image img;
   DBG << widthpx << "x" << heightpx <<":"<<T.DATA.size();
   img.create(widthpx, heightpx, data);
@@ -28,9 +30,14 @@ int main()
   texture.loadFromImage(img);
   DBG << "Texture: " << texture.getSize().x << "x" << texture.getSize().y;
   shape.setTexture(&texture);
-  shape.setPosition((window.getSize().x - shape.getSize().x)/2, (window.getSize().y - shape.getSize().y)/2);
+  auto shapeMid = [&]()
+  {
+    return sf::Vector2f((window.getSize().x - shape.getSize().x)/2, (window.getSize().y - shape.getSize().y)/2);
+  };
+  shape.setPosition(shapeMid());
   while (window.isOpen())
   {
+    shape.scale(1,1);
     sf::Event event;
     while (window.pollEvent(event))
     {
@@ -39,15 +46,26 @@ int main()
       if (event.type == sf::Event::MouseWheelScrolled)
       {
         shape.setSize(sf::Vector2f(shape.getSize().x+event.mouseWheelScroll.delta*SCROLLFACTOR, shape.getSize().y+event.mouseWheelScroll.delta*SCROLLFACTOR));
-        DBG << window.getSize().x << "x" << window.getSize().y;
-        shape.setPosition((window.getSize().x - shape.getSize().x)/2, (window.getSize().y - shape.getSize().y)/2);
+        DBG << "Window: " << window.getSize().x << "x" << window.getSize().y;
+        DBG << "Shape Size: " << shape.getSize().x << "x" << shape.getSize().y;
+        DBG << "Shape Position: "<< shape.getPosition().x << "x" << shape.getPosition().y;
+        shape.setPosition(shapeMid());
+      }
+      if(event.type == sf::Event::Resized)
+      {
+        // when the window is resized, the mappings get out of scope. Remap them to our current X,Y coords
+        window.setView(sf::View(sf::FloatRect(0, 0, event.size.width, event.size.height)));
+        shape.setPosition(shapeMid());
       }
       if(event.type == sf::Event::KeyPressed)
       {
         switch(event.key.code)
         {
           case sf::Keyboard::Escape:
-            shape.setSize(sf::Vector2f(static_cast<float>(window.getSize().x)/2, static_cast<float>(window.getSize().y)/2));
+            window.setSize(Windowpx);
+            window.setView(sf::View(sf::FloatRect(0, 0, Windowpx.x, Windowpx.y)));
+            shape.setSize(Oldshape);
+            shape.setPosition(shapeMid());
             break;
           case sf::Keyboard::Q:
             return EXIT_SUCCESS;
