80d1f82457bb5b1fb02737b5d01fa041ed0f7d68
Darksider3 <github@darksider3.de>
2019-12-05 08:30:55 +0100

    Iterators! \o

--- LiGi/LinkedList.h
+++ LiGi/LinkedList.h
@@ -5,13 +5,6 @@
 #include "Assertions.h"
 namespace Li
 {
-
-template<typename T>
-class LLIterator
-{
-
-};
-
 template<typename T>
 class LLNode
 {
@@ -61,20 +54,94 @@ private:
   T* m_prev;
 };
 
+// FIXME: Not working right, somehow misses the start but goes directly to the end. Will see how that works later
+template<typename T>
+class LLIterator
+{
+public:
+  LLIterator(T* ptr)
+  {
+    m_current = ptr;
+  }
+
+  LLIterator& operator=(LLNode<T> *ptr)
+  {
+    assert(ptr);
+    m_current = static_cast<LLNode<T*>>(ptr);
+    return *this;
+  }
+
+  LLIterator& operator++()
+  {
+    assert(m_current->next());
+    if(m_current)
+    {
+      m_current = m_current->next();
+    }
+    return *this;
+  }
+
+  LLIterator& operator++(int) // postfix?
+  {
+    LLIterator<T> it = *this;
+    ++*this;
+    return it;
+  }
+
+  bool operator!=(const LLIterator<T> &it)
+  {
+    return m_current != it.m_current;
+  }
+
+  int operator*()
+  {
+    return m_current;
+  }
+
+  const T* m_current;
+};
+
+
 template<typename T>
 class LinkedList
 {
 public:
   LinkedList()
   {
-    m_tester = std::make_unique<T>();
-    m_tester->data = 1;
+  }
+
+  bool empty()
+  {
+    return (m_head == nullptr);
+  }
+
+  void count(bool t)
+  {
+    m_store=t;
+    if(m_store)
+    {
+      m_counter = 0;
+      m_counter = size();
+    }
   }
 
   T* head() { return m_head; }
+  T* tail() { return m_tail; }
+
+  T* begin()
+  {
+    return m_head;
+  }
+
+  T* end()
+  {
+    return m_tail;
+  }
   void append(T* Node)
   {
     assert(Node);
+    if(m_store)
+      ++m_counter;
     if(m_head == nullptr)
     {
       m_head = Node;
@@ -94,6 +161,8 @@ public:
   void prepend(T* Node)
   {
     assert(Node);
+    if(m_store)
+      ++m_counter;
     if(!m_head)
     {
       m_head = Node;
@@ -119,6 +188,12 @@ public:
     return count;
   }
 
+  size_t counter()
+  {
+    if(!m_store)
+      return size();
+    return m_counter;
+  }
   void sort()
   {
     /*
@@ -138,6 +213,8 @@ public:
 private:
   T* m_tail { nullptr };
   T* m_head { nullptr };
+  bool m_store{false};
+  size_t m_counter{0};
 
 };
 
