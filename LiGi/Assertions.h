a5b87cfae8ca4e2e170d0ac3f6cb7913a9bc43bd
Darksider3 <github@darksider3.de>
2019-12-05 08:30:37 +0100

    Assert \o

--- LiGi/Assertions.h
+++ LiGi/Assertions.h
@@ -1,15 +1,14 @@
 #pragma once
 #include <cstdlib>
 #include <cstdio>
-#undef assert
-
 #ifdef DEBUG
+#undef assert
 __attribute__((noreturn)) void __assertion_failed(const char* msg, const char* file, unsigned line, const char* func);
 #    define assert(expr) ((expr) ? (void)0 : __assertion_failed(#    expr, __FILE__, __LINE__, __PRETTY_FUNCTION__))
 #    define msgassert(expr, msg) ((expr) ? (void)0 : __assertion_failed(msg, __FILE__, __LINE__, __PRETTY_FUNCTION__))
 #    define ASSERT_NOT_REACHED() assert(false)
 #else
-#    define assert(expr) void()
+#    define owassert(expr) void()
 #    define msgassert(expr, msg) void()
 #    define ASSERT_NOT_REACHED() void()
 #endif
