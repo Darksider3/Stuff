d53407f253b4e6d6a69eee55da936cc97fcbd419
Darksider3 <github@darksider3.de>
2019-12-06 16:13:05 +0100

    ErrorCase, run functions

--- LiGi/Test.h
+++ LiGi/Test.h
@@ -6,21 +6,39 @@
 #include <string>
 #include <memory>
 
+namespace Li
+{
+
+struct ErrorCase
+{
+  int errorCode;
+  std::string errorDesc;
+};
+
 struct TestCase : public Li::LLNode<TestCase>
 {
-  void (*foo)();
+  bool (*foo)();
   std::string descr;
-  std::string category = "default";
-  short success = false; // true, false, 2->forced true
-  bool run = false;
+  std::string category {"default"};
+  short success {false}; // true, false, 2->forced true
+  bool run {false};
+  ErrorCase *error {nullptr};
 };
 
 //@todo: Test suite!
 // It should be able to run every test, and produce a result-screen out of it
 // nothing heavy, probably something with the Assertion.h
-class Tests : Li::LinkedList<TestCase>
+class Tests : public Li::LinkedList<TestCase>
 {
-  void run_all(){}
+
+public:
+  void run_all()
+  {
+    for(auto it = begin(); it != end(); ++it)
+    {
+      bool ret { it.current->foo() };
+    }
+  }
   void mark_runned(TestCase *m)
   {
     m->success = 2;
@@ -40,5 +58,5 @@ class Tests : Li::LinkedList<TestCase>
     return true;
   }
 };
-
+}
 #endif // TEST_H
