05fa73ef2e25e5fe01c3666ced25d9fc530c47e2
Darksider3 <github@darksider3.de>
2020-05-01 14:23:42 +0200

    DB relevant changes
    Setting up the first relevant database settings and storing it in the
    already accurate file. Looking right now to integrate it properly(TM)

--- Python/WinRateTwichFightsPille/venv/app/config.py
+++ Python/WinRateTwichFightsPille/venv/app/config.py
@@ -1,1 +1,14 @@
 DBG_ENABLED = True
+
+WIN_STR = "won"
+LOST_STR = "lost"
+
+DB_PATH = "/home/darksider3/Desktop/winrate.db"
+
+DB_CREATION = """
+CREATE TABLE "WinRate" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"time"	INTEGER DEFAULT CURRENT_TIMESTAMP,
+	"againstName"	TEXT,
+	"outcome"	TEXT DEFAULT 'win'
+)"""
