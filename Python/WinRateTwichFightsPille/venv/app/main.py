4891787324f26e44319610d4f4b98836580cfd86
Darksider3 <github@darksider3.de>
2020-05-01 14:24:30 +0200

    SQLite Connection sceleton and simple insertion
    Have to admit this is fairly little all in all, but will provide a solid
    foundation i'd like to build on

--- Python/WinRateTwichFightsPille/venv/app/main.py
+++ Python/WinRateTwichFightsPille/venv/app/main.py
@@ -3,8 +3,30 @@ from config import *
 from dbg import *
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit
 from PyQt5.QtCore import Qt
+import sqlite3
 
+class sqliteHandler():
+    Con: sqlite3.Connection
+    Curs: sqlite3.Cursor
 
+    def __init__(self, path: str=DB_PATH):
+        try:
+            self.Con = sqlite3.connect(path)
+            self.Curs = self.Con.cursor()
+        except sqlite3.Error as e:
+            print("Couldn't connect to database...")
+
+    def insert(self, against: str, outcome: str):
+        insertion: str = "INSERT INTO WinRate (againstName, outcome) VALUES (?, ?);"
+        self.Curs.execute(insertion, tuple([against, outcome]))
+        self.Con.commit()
+
+    def __del__(self):
+        try:
+            self.Con.commit()
+            self.Con.close()
+        except sqlite3.Error as e:
+            print("Couldn't close DB")
 
 class MainWindow(QWidget):
     def __init__(self, app: QApplication):
@@ -13,10 +35,12 @@ class MainWindow(QWidget):
         self.Layout = QVBoxLayout()
         self.Layout.addWidget(QPushButton("Push"))
         self.setLayout(self.Layout)
+        self.SQLite = sqliteHandler()
+        self.SQLite.insert("testuser", "won")
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
-            dbg("Escape press, exiting...")
+            dbg("Escape pressed, exiting...")
             self.app.exit()
         else:
             dbg(f"irrelevant keypress gets passed through with key {event.key()}")
