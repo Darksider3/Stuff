ae65e0738f914245564c0b3c467c2fd1b4b30b79
Darksider3 <github@darksider3.de>
2020-05-01 08:23:09 +0200

    class mainwindow sceleton suits us better here...

--- Python/WinRateTwichFightsPille/venv/app/main.py
+++ Python/WinRateTwichFightsPille/venv/app/main.py
@@ -1,12 +1,27 @@
+import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit
-app = QApplication([])
-InputBox = QLineEdit("Input")
-window = QWidget()
-layout = QVBoxLayout()
-layout.addWidget(QPushButton("Top"))
-layout.addWidget(QPushButton("Bottom"))
-layout.addWidget(InputBox)
-window.setLayout(layout)
-window.show()
-app.beep()
-app.exec_()
+from PyQt5.QtCore import Qt
+
+import SQL
+
+
+class MainWindow(QWidget):
+    def __init__(self, app: QApplication):
+        super().__init__()
+        self.app = app
+        self.Layout = QVBoxLayout()
+        self.Layout.addWidget(QPushButton("Push"))
+        self.setLayout(self.Layout)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            print("Escape press, exiting...")
+            self.app.exit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = MainWindow(app)
+    window.show()
+    app.exec_()
