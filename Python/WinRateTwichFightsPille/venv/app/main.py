5825c7a880fdcdd06988c42674774df1e2804543
Darksider3 <github@darksider3.de>
2020-05-03 11:33:16 +0200

    Implements 2 input lines for state and username.
    And adds some basic completion done by Qt itself.
    TODO is to actually validate at least the state. Allowed values shall be
    set in the config.py file.

--- Python/WinRateTwichFightsPille/venv/app/main.py
+++ Python/WinRateTwichFightsPille/venv/app/main.py
@@ -1,7 +1,7 @@
 import sys
 from config import *
 from dbg import *
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QCompleter
 from PyQt5.QtCore import Qt
 import sqlite3
 
@@ -13,6 +13,7 @@ class sqliteHandler():
         try:
             self.Con = sqlite3.connect(path)
             self.Curs = self.Con.cursor()
+            self.Curs.row_factory = sqlite3.Row
         except sqlite3.Error as e:
             print("Couldn't connect to database...")
 
@@ -20,6 +21,18 @@ class sqliteHandler():
         insertion: str = "INSERT INTO WinRate (againstName, outcome) VALUES (?, ?);"
         self.Curs.execute(insertion, tuple([against, outcome]))
         self.Con.commit()
+
+    def getNameCompleteList(self):
+        completer_list_query: str = "SELECT `againstName` FROM WinRate"
+        completer_query_result = self.Curs.execute(completer_list_query)
+        completer_query_result = completer_query_result.fetchall()
+        self.Con.commit()
+        return_list = list()
+        for key in completer_query_result:
+            if not key["againstName"] in return_list:
+                return_list.append(key["againstName"])
+
+        return return_list
 
     def __del__(self):
         try:
@@ -31,11 +44,21 @@ class sqliteHandler():
 class MainWindow(QWidget):
     def __init__(self, app: QApplication):
         super().__init__()
+        self.SQLite = sqliteHandler()
+        completer_list = self.SQLite.getNameCompleteList()
+        dbg(completer_list)
+
+        self.username = QLineEdit("Username....")
+        self.username.setCompleter(QCompleter(completer_list, self.username))
+        self.fight_state = QLineEdit("won") # @TODO: Validator. Just accept won/lost(or any other value set in cfg)
+        self.fight_state.setCompleter(QCompleter(["won", "lost"], self.fight_state))
+
         self.app = app
         self.Layout = QVBoxLayout()
-        self.Layout.addWidget(QPushButton("Push"))
+        self.Layout.addWidget(self.username)
+        self.Layout.addWidget(self.fight_state)
+        self.Layout.addWidget(QPushButton("Register"))
         self.setLayout(self.Layout)
-        self.SQLite = sqliteHandler()
         #self.SQLite.insert("testuser", "won")
 
     def keyPressEvent(self, event):
@@ -46,6 +69,9 @@ class MainWindow(QWidget):
             dbg(f"irrelevant keypress gets passed through with key {event.key()}")
             super().keyPressEvent(event)
 
+    def on_submitToDatabase(self):
+        user=self.username.text()
+        state=self.fight_state.text()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
