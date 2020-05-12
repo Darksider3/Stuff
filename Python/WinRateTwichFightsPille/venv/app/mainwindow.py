b0d8fb58a4cbfed857793bd839f452ecfbe319f6
Darksider3 <github@darksider3.de>
2020-05-12 09:56:21 +0200

    mainwindow: Make use of lambda, which eliminates some variables in class
    Notable self.username and self.fight_state can be both now scoped just
    in the __init__ function because we don't have to access them outside
    anymore.
    
    PyQt5 actually can supply arguments to functions in connect()-events
    when you take the calling function and wrap it into an lambda

--- Python/WinRateTwichFightsPille/venv/app/mainwindow.py
+++ Python/WinRateTwichFightsPille/venv/app/mainwindow.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import Qt, pyqtSlot
 from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit, QCompleter, QPushButton
 
 import util
+from config import USERNAME_PLACEHOLDER
 from dbg import dbg
 from stats_window import StatisticsWindow
 from fightstatevalidator import QFightStateValidator
@@ -24,22 +25,22 @@ class MainWindow(QWidget):
         dbg(percentWin)
         # @TODO: Evaluate if better done in a separate Module, inheriting QLineEdit
         # @TODO: Spaghettifull
-        self.username = QLineEdit("Username....", self)
-        self.username.setFixedWidth(100)
-        self.username.move(0, 10)
-        self.username_completer = QCompleter(userlist, self.username)
-        self.username_completer.setCompletionMode(QCompleter.InlineCompletion)
-        self.username.setCompleter(self.username_completer)
-        self.fight_state = QLineEdit("won", self)
-        self.fight_state.setValidator(QFightStateValidator())
-        self.fight_state.setFixedWidth(100)
-        self.fight_state.setCompleter(QCompleter(["won", "lost"], self.fight_state))
-        self.fight_state.move(0,40)
+        username = QLineEdit(USERNAME_PLACEHOLDER, self)
+        username.setFixedWidth(100)
+        username.move(0, 10)
+        username_completer = QCompleter(userlist, username)
+        username_completer.setCompletionMode(QCompleter.InlineCompletion)
+        username.setCompleter(username_completer)
+        fight_state = QLineEdit("won", self)
+        fight_state.setValidator(QFightStateValidator())
+        fight_state.setFixedWidth(100)
+        fight_state.setCompleter(QCompleter(["won", "lost"], fight_state))
+        fight_state.move(0,40)
 
         insert_button = QPushButton("Register", self)
         insert_button.setFixedWidth(80)
         insert_button.move(0, 80)
-        insert_button.clicked.connect(self.on_submitToDatabaseClick)
+        insert_button.clicked.connect(lambda: self.on_submitToDatabaseClick(username.text(), fight_state.text()))
 
         update_button = QPushButton("Update", self)
         update_button.setFixedWidth(80)
@@ -71,8 +72,6 @@ class MainWindow(QWidget):
             super().keyPressEvent(event)
 
     @pyqtSlot()
-    def on_submitToDatabaseClick(self):
-        user = self.username.text()
-        state = self.fight_state.text()
+    def on_submitToDatabaseClick(self, user, state):
         dbg(f"inserting user '{user}' with fightstate '{state}'")
         self.SQLite.insert(user, state)
