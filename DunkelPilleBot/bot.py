dcaef0a19a87411057fe89e1ac050e0cd6a6f565
Darksider3 <github@darksider3.de>
2020-07-24 20:31:53 +0200

    PilleBot: More functionality for automatic events! :)

--- DunkelPilleBot/bot.py
+++ DunkelPilleBot/bot.py
@@ -8,10 +8,14 @@ import random
 BEEF_STRINGS = ["!beef bastelRage2", "!beef", "!beef pillebMOPS", "!beef Clap",
                 "!beef monkaS", "!beef monkaT", "!beef"]
 
-IN_BEEF = False;
-EARLIEST_NEXT_BEEF = datetime.datetime.now() - datetime.timedelta(minutes=5);
+MOPS_STRINGS = ["!mops bastelRage2", "!mops", "!mops pillebMOPS", "!mops Clap",
+                "!mops monkaS", "!mops monkaT", "!mops", "!mops ROLLMOPS "]
 
-assert(EARLIEST_NEXT_BEEF < datetime.datetime.now())
+EARLIEST_NEXT_BEEF = datetime.datetime.now() + datetime.timedelta(minutes=2)
+EARLIEST_NEXT_KARAOKE = datetime.datetime.now() + datetime.timedelta(minutes=3)
+EARLIEST_NEXT_MOPS = datetime.datetime.now() + datetime.timedelta(minutes=1)
+
+assert(EARLIEST_NEXT_BEEF > datetime.datetime.now())
 
 bot = commands.Bot(
     # set up the bot
@@ -33,18 +37,35 @@ async def event_ready():
 async def event_message(ctx):
     global IN_BEEF
     global EARLIEST_NEXT_BEEF
+    global EARLIEST_NEXT_KARAOKE
+    global EARLIEST_NEXT_MOPS
     global BEEF_STRINGS
     'Runs every time a message is sent in chat.'
     # make sure the bot ignores itself and the streamer
     #if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
     #    return
     if "!beef" in ctx.content: 
-        if EARLIEST_NEXT_BEEF < datetime.datetime.now() and (ctx.author.name not in "pilloobot"):
+        if EARLIEST_NEXT_BEEF < datetime.datetime.now():
+            if "pilloobot" not in ctx.author.name and "Dunkelseiter" not in ctx.author.name:
+                time.sleep(2)
+                await ctx.channel.send(random.choice(BEEF_STRINGS))
+                EARLIEST_NEXT_BEEF=datetime.datetime.now() + datetime.timedelta(minutes=6)
+                print(f"{datetime.datetime.now()} - DEBUG: BEEF TRIGGERED")
+                
+    if "!karaoke" in ctx.content: 
+        if EARLIEST_NEXT_KARAOKE < datetime.datetime.now():
+            if "pilloobot" not in ctx.author.name and "Dunkelseiter" not in ctx.author.name:
+                time.sleep(2)
+                await ctx.channel.send("!karaoke 666")
+                EARLIEST_NEXT_KARAOKE=datetime.datetime.now() + datetime.timedelta(minutes=10)
+                print(f"{datetime.datetime.now()} - DEBUG: KARAOKE TRIGGERED")
+                
+    if EARLIEST_NEXT_MOPS < datetime.datetime.now():
+        if "pilloobot" not in ctx.author.name and "Dunkelseiter" not in ctx.author.name:
             time.sleep(2)
-            await ctx.channel.send(random.choice(BEEF_STRINGS))
-            EARLIEST_NEXT_BEEF=datetime.datetime.now() + datetime.timedelta(minutes=8)
-            print(f"{datetime.datetime.now()} - DEBUG: BEEF TRIGGERED")
-            
+            await ctx.channel.send(random.choice(MOPS_STRINGS))
+            EARLIEST_NEXT_MOPS=datetime.datetime.now() + datetime.timedelta(minutes=3)
+            print(f"{datetime.datetime.now()} - DEBUG: MOPS TRIGGERED")
 
 
 # bot.py
