1230a6dae4dc6a9449d4b5166641aa6a31ed0732
Darksider3 <github@darksider3.de>
2020-07-27 16:00:51 +0200

    LiGi-Timer-test-client: little steps towards pomodoro counter

--- LiGi/timer.cpp
+++ LiGi/timer.cpp
@@ -1,9 +1,18 @@
 #include "timer.h"
 
-int main()
+#define ERASELINE printf("\033[A\33[2K");
+#define THREAD_DONE true;
+#define THREAD_RUNNING false;
+
+bool DONE = false;
+int Pomos = 0;
+
+
+void Pomo()
 {
+  DONE = THREAD_RUNNING;
   std::atomic_bool Stop(false);
-  auto Goal = std::chrono::milliseconds(5000);
+  auto Goal = std::chrono::milliseconds(1000*60*30);
   Timer1 timer1(Stop, callout, Goal);
 
   std::thread thread1(&Timer1::RunnerFunc, &timer1);
@@ -20,15 +29,37 @@ int main()
       std::cout << curElapsed;
       break;
     }
+    std::cout << "curElapsed: " << curElapsed << ", uintGoal: " << uintGoal << ", Percent done: " <<
+                 (curElapsed / (uintGoal/100))<< "\n";
     timer1.elapsed_lock();
     curElapsed = timer1.get_elapsed();
     timer1.elapsed_unlock();
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    std::cout << "curElapsed: " << curElapsed << ", uintGoal: " << uintGoal << std::endl;
+    ERASELINE
   }
 
+
+  DONE = THREAD_DONE;
+  ++Pomos;
   Stop = true;
   thread1.join();
+}
 
+int main()
+{
+  std::string answer("");
+  while(true)
+  {
+    Pomo();
+    if(DONE)
+    {
+      std::cout << "Would you like to start a new pomodoro? You have done " << Pomos << " already: ";
+      std::cin >> answer;
+      if(answer == "yes")
+        continue;
+      else
+        break;
+    }
+  }
   return 0;
 }
