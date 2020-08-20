877bd919a43fc4f8aef2b31309ac51c97e844982
Darksider3 <github@darksider3.de>
2020-08-20 16:17:06 +0200

    LiGi-TimerTools: Running without adding values...

--- LiGi/TimerTools.h
+++ LiGi/TimerTools.h
@@ -12,15 +12,22 @@ template<Li::Literals::TimeValue T>
 std::string getSeconds(T const &t)
 {
   std::stringstream ss("");
-  T secs = (
-               (t + 100)
-               / 1000)
-             % 60;
-  if(secs < 10)
-    ss << "0" << secs;
+  T secs;
+  if(t == 0 || t == 1)
+  {
+    ss << "00";
+  }
   else
-    ss << secs;
-
+  {
+    secs =    (
+                t
+                / 1000)
+              % 60;
+    if(secs < 10)
+      ss << "0" << secs;
+    else
+      ss << secs;
+  }
   return ss.str();
 }
 
@@ -28,16 +35,22 @@ template<Li::Literals::TimeValue T>
 std::string getMinutes(T const &t)
 {
   std::stringstream ss("");
-  T mins = ((
-                (t + 100)
-                / 1000)
-              / 60)
-             % 60;
-  if(mins < 10)
-    ss << "0" << mins;
+  T mins;
+  if(t == 0 || t == 1)
+  {
+    ss << "00";
+  }
   else
-    ss << mins;
-
+  {
+    mins = ((
+              t / 1000)
+            / 60)
+           % 60;
+    if(mins < 10)
+      ss << "0" << mins;
+    else
+      ss << mins;
+  }
   return ss.str();
 }
 
@@ -45,25 +58,32 @@ template<Li::Literals::TimeValue T>
 std::string getHours(T const &t)
 {
   std::stringstream ss("");
-  T hours = (((
-                  (t + 100)
-                  / 1000)
-                / 60)
-               / 60)
-              % 60;
-  if(hours < 10)
-    ss << "0" << hours;
-  else
-    ss << hours;
+  T hours;
 
+  if(t == 0 || t == 1)
+  {
+    ss << "00";
+  }
+  else
+  {
+    hours =  (((    t
+                    / 1000)
+               / 60)
+              / 60)
+             % 60;
+    if(hours < 10)
+      ss << "0" << hours;
+    else
+      ss << hours;
+  }
   return ss.str();
 }
 template<Li::Literals::TimeValue T>
 std::string getFullTimeString(T const &t)
 {
   return (
-            getHours(t)+":"+getMinutes(t)+":"+getSeconds(t)
-            );
+        getHours(t)+":"+getMinutes(t)+":"+getSeconds(t)
+        );
 }
 }
 }
