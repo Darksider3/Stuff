206a785be747d16b54e16c704a94321ec7cff613
Darksider3 <github@darksider3.de>
2020-08-26 16:11:56 +0200

    LiGi-App-Playground: Threadtest intendantion and scoped locks

--- LiGi/apps/ncurses_playground/ThreadTest.cpp
+++ LiGi/apps/ncurses_playground/ThreadTest.cpp
@@ -5,112 +5,125 @@
 #include <mutex>
 #include <deque>
 
+namespace Li
+{
+namespace FuncModifiers
+{
+namespace Thread
+{
+  enum __ShareThread{} ShareThread; // Selector
+  enum __HoldThread{} HoldThread;
+}
+}
+}
 
-  class Threader
+class ThreadWrapper
+{
+private:
+  std::deque<std::function<void()>> funcQueue;
+  std::mutex queueLock, statelock;
+  std::atomic_bool &globalStop;
+  enum anonym
   {
-  private:
-    std::deque<std::function<void()>> funcQueue;
-    std::mutex queueLock, statelock;
-    std::atomic_bool &globalStop;
-    enum anonym
+    idle,
+    running
+  } mState = idle;
+
+  void threadingFunction()
+  {
+    while(!this->globalStop)
     {
-      idle,
-      running
-    } mState = idle;
-
-    void threadingFunction()
-    {
-        while(!this->globalStop)
-        {
-          // @todo additional scope for scoped_lock
-          {
-            std::scoped_lock<std::mutex>(this->queueLock);
-            if(!this->funcQueue.empty())
-            {
-              runFunc();
-            }
-            else
-            {
-              std::cout << "idling" << std::endl;
-              std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            }
-          }
-        }
-    }
-
-  public:
-
-    explicit Threader(std::atomic_bool &GlobalStopVar) : globalStop(GlobalStopVar)
-    {}
-
-    template<typename FUNC, typename ... Arguments>
-    bool insert(FUNC f, Arguments&&... args)
-    {
-      {
-        std::scoped_lock<std::mutex> l(this->queueLock);
-        if(this->funcQueue.size() > 0)
-          return false; // just one element allowed
-      }
-      std::function<void()> Next = std::function<void()>(
-                                                 std::bind(f, std::forward<Arguments>(args)...));
+      // @todo additional scope for scoped_lock
       {
         std::scoped_lock<std::mutex>(this->queueLock);
-        this->funcQueue.emplace_back(std::move(Next));
+        if(!this->funcQueue.empty())
+        {
+          runFunc();
+        }
+        else
+        {
+          std::cout << "idling" << std::endl;
+          std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
       }
-      return true;
     }
+  }
+protected:
+public:
 
+  explicit ThreadWrapper(std::atomic_bool &GlobalStopVar) : globalStop(GlobalStopVar)
+  {}
 
-
-    std::shared_ptr<std::thread> RunThread(Threader &)
+  template<typename FUNC, typename ... Arguments>
+  bool insert(FUNC f, Arguments&&... args)
+  {
     {
-      std::shared_ptr<std::thread> &&MovingThread = std::make_shared<std::thread>(&Threader::threadingFunction, std::ref(*this));
-      return std::move(MovingThread);
+      std::scoped_lock<std::mutex> l(this->queueLock);
+      if(this->funcQueue.size() > 0)
+        return false; // just one element allowed
     }
-    bool runFunc()
+    std::function<void()> Next = std::function<void()>(
+                                   std::bind(f, std::forward<Arguments>(args)...));
     {
-      std::function<void()> RunFunc;
-      {
-        std::scoped_lock<std::mutex> (this->queueLock);
-        if(this->funcQueue.size() < 1)
-          return false;
-        RunFunc = this->funcQueue.back();
-      }
-      {
-        std::scoped_lock<std::mutex> (this->statelock);
-        mState = running;
-      }
-
-      RunFunc();
-
-      {
-        std::scoped_lock<std::mutex> (this->statelock);
-        mState = idle;
-      }
-
-      {
-        std::scoped_lock<std::mutex> (this->queueLock);
-        this->funcQueue.pop_back();
-      }
-      return true;
+      std::scoped_lock<std::mutex>(this->queueLock);
+      this->funcQueue.emplace_back(std::move(Next));
     }
+    return true;
+  }
 
-    void printTypeLol()
+
+
+  std::shared_ptr<std::thread> RunThread(Li::FuncModifiers::Thread::__ShareThread&)
+  {
+    // std::ref(*this): We want the Thread to have our data here...
+    std::shared_ptr<std::thread> &&MovingThread = std::make_shared<std::thread>(&ThreadWrapper::threadingFunction, std::ref(*this));
+    return std::move(MovingThread);
+  }
+
+  bool runFunc()
+  {
+    std::function<void()> RunFunc;
     {
-      for(auto &b: this->funcQueue)
-        std::cout << typeid(b).name() << std::endl;
+      std::scoped_lock<std::mutex> (this->queueLock);
+      if(this->funcQueue.size() < 1)
+        return false;
+      RunFunc = this->funcQueue.back();
+    }
+    {
+      std::scoped_lock<std::mutex> (this->statelock);
+      mState = running;
     }
 
-    bool is_running()
+    RunFunc();
+
     {
-      std::scoped_lock<std::mutex>(this->statelock);
-      return (this->mState == running);
+      std::scoped_lock<std::mutex> (this->statelock);
+      mState = idle;
     }
 
-    ~Threader()
     {
+      std::scoped_lock<std::mutex> (this->queueLock);
+      this->funcQueue.pop_back();
     }
-  };
+    return true;
+  }
+
+  void printTypeLol()
+  {
+    for(auto &b: this->funcQueue)
+      std::cout << typeid(b).name() << std::endl;
+  }
+
+  bool is_running()
+  {
+    std::scoped_lock<std::mutex>(this->statelock);
+    return (this->mState == running);
+  }
+
+  ~ThreadWrapper()
+  {
+  }
+};
 
 
 int main()
@@ -125,8 +138,8 @@ int main()
   };
 
   std::atomic_bool Global = false;
-  Threader ThreaderWrapper(Global);
-  std::shared_ptr<std::thread> ThreadCopy = ThreaderWrapper.RunThread(ThreaderWrapper);
+  ThreadWrapper ThreaderWrapper(Global);
+  std::shared_ptr<std::thread> ThreadCopy = ThreaderWrapper.RunThread(Li::FuncModifiers::Thread::ShareThread);
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
   ThreaderWrapper.insert<void()>(blafunc);
   std::string asw = "";
