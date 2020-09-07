600fc176c66494f78bcbe5b12bb818d5a7c7d569
Darksider3 <github@darksider3.de>
2020-09-07 14:21:00 +0200

    LiGi-GeneralTools: TODO-Brainstormed-Overview

--- LiGi/GeneralTools.h
+++ LiGi/GeneralTools.h
@@ -22,6 +22,27 @@ std::vector<std::string> split(const std::string& s, const char delimiter) noexc
   return tokens;
 }
 
+
+/* @TODO: Generalized Parser for easy of use on the other parsers im going to write... ._,
+ */
+/* @TODO: URN, URI, URL-Parser
+ * URI: https://tools.ietf.org/html/rfc3986
+ * URL: Edit-Distance maybe helpfull by misspelling?
+ * URN: Scheme://authority:AuthorityPort/PathOnAuthority?query=Value#Fragment
+ */
+
+/* @TODO: Filesystem: dynamic is_absolute(), is_relative() just for excercise
+ * POSIX defines absolute anything with / in front as absolute, anything else is pretty much relative
+ * ... but URL/URIs are still allowed! Support? Scheme:/// <- Absolute Scheme://bla/ <- Absolute
+ */
+/*
+ * @TODO: Filesystem class!
+ *  -> 1. Li::fs::Path first! -> {get|is}_absolute, {get|is}_relative, "auto guess path type", is_{dir|file|pipe|...}, can_{read, write,exec, del}
+ *  -> 2. Li::fs::GeneralFile -> Abstraction with common parts across all file-types
+ *  -> 3. Li::fs::FIFO        -> FIFO-Handle&Help!
+ *  -> 4. Li::fs::Pipe        -> .^^
+ *  -> 5. Followed by.. TBA
+ */
 }
 }
 
