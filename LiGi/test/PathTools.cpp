d69e3a4441679690f755e4a0d0e22dfee27c0f2f
Darksider3 <github@darksider3.de>
2020-09-25 17:36:57 +0200

    FP_or_error Implementation

--- LiGi/test/PathTools.cpp
+++ LiGi/test/PathTools.cpp
@@ -13,14 +13,7 @@ bool test_file_exists(Li::TestCase* tcase)
         returner = false;
         tcase->errorDesc = "/etc/passwd not present! :o";
     }
-    return returner;
-}
 
-bool test_file_doesnt_exists(Li::TestCase* tcase)
-{
-    bool returner = true;
-    tcase->name = "File_exists but intentionally failing";
-    tcase->descr = "See name";
     if (Li::GeneralTools::fs::file_exists("humbug")) {
         tcase->error = 1;
         returner = false;
@@ -29,18 +22,47 @@ bool test_file_doesnt_exists(Li::TestCase* tcase)
     return returner;
 }
 
+bool test_fp_or_error(Li::TestCase* tcase)
+{
+    bool returner = true;
+    tcase->name = "fp_or_error";
+    tcase->descr = "Test filepointer and errno coverage";
+
+    auto f = Li::GeneralTools::fs::error_or_fp("./pathtools_test");
+    if (!f.first.first) {
+        tcase->error = 1;
+        returner = false;
+        tcase->errorDesc = "Somehow we couldn't open this exact binary and write to it despite building it!";
+        if (f.second != nullptr) {
+            tcase->errorDesc += "\nBut somehow we aquired a somewhat valid file pointer?!";
+            tcase->error = 2;
+        }
+
+        tcase->errorDesc += "Error message: " + std::string(f.first.second);
+    }
+
+    f = Li::GeneralTools::fs::error_or_fp("/etc/pa/sodkasl/dk");
+    if (f.first.first) {
+        tcase->error = 1;
+        tcase->errorDesc = "This file shouldn't exist ANYWHERE on this planet. Means, the function is misguided...";
+        returner = false;
+    }
+
+    return returner;
+}
+
 int main()
 {
     Li::Test* tester = Li::Test::instance();
 
     std::shared_ptr<Li::TestCase> TestFileExists = std::make_shared<Li::TestCase>();
-    std::shared_ptr<Li::TestCase> TestFileNotExists = std::make_shared<Li::TestCase>();
+    std::shared_ptr<Li::TestCase> TestFPOrError = std::make_shared<Li::TestCase>();
 
     TestFileExists->func = *test_file_exists;
-    TestFileNotExists->func = *test_file_doesnt_exists;
+    TestFPOrError->func = *test_fp_or_error;
 
     tester->append(TestFileExists.get());
-    tester->append(TestFileNotExists.get());
+    tester->append(TestFPOrError.get());
 
     tester->exec();
 
