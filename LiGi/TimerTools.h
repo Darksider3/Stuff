8e4115c4745933608ba56b379ac40ba22524de6c
Darksider3 <github@darksider3.de>
2020-10-09 16:09:18 +0200

    General reformatting of timer.h and TimerTools. Member naming.

--- LiGi/TimerTools.h
+++ LiGi/TimerTools.h
@@ -1,89 +1,75 @@
 #ifndef TIMERTOOLS_H
 #define TIMERTOOLS_H
 #include "timer.h"
-namespace Li
-{
-namespace TimerTools
-{
-namespace Format
-{
+namespace Li {
+namespace TimerTools {
+namespace Format {
 
 template<Li::Literals::TimeValue T>
-std::string getSeconds(T const &t)
+std::string getSeconds(T const& t)
 {
-  std::stringstream ss("");
-  T secs;
-  if(t == 0 || t == 1)
-  {
-    ss << "00";
-  }
-  else
-  {
-    secs =    (
-                t
-                / 1000)
-              % 60;
-    if(secs < 10)
-      ss << "0" << secs;
-    else
-      ss << secs;
-  }
-  return ss.str();
+    std::stringstream ss("");
+    T secs;
+    if (t == 0 || t == 1) {
+        ss << "00";
+    } else {
+        secs = (t
+                   / 1000)
+            % 60;
+        if (secs < 10)
+            ss << "0" << secs;
+        else
+            ss << secs;
+    }
+    return ss.str();
 }
 
 template<Li::Literals::TimeValue T>
-std::string getMinutes(T const &t)
+std::string getMinutes(T const& t)
 {
-  std::stringstream ss("");
-  T mins;
-  if(t == 0 || t == 1)
-  {
-    ss << "00";
-  }
-  else
-  {
-    mins = ((
-              t / 1000)
-            / 60)
-           % 60;
-    if(mins < 10)
-      ss << "0" << mins;
-    else
-      ss << mins;
-  }
-  return ss.str();
+    std::stringstream ss("");
+    T mins;
+    if (t == 0 || t == 1) {
+        ss << "00";
+    } else {
+        mins = ((
+                    t / 1000)
+                   / 60)
+            % 60;
+        if (mins < 10)
+            ss << "0" << mins;
+        else
+            ss << mins;
+    }
+    return ss.str();
 }
 
 template<Li::Literals::TimeValue T>
-std::string getHours(T const &t)
+std::string getHours(T const& t)
 {
-  std::stringstream ss("");
-  T hours;
+    std::stringstream ss("");
+    T hours;
 
-  if(t == 0 || t == 1)
-  {
-    ss << "00";
-  }
-  else
-  {
-    hours =  (((    t
-                    / 1000)
-               / 60)
-              / 60)
-             % 60;
-    if(hours < 10)
-      ss << "0" << hours;
-    else
-      ss << hours;
-  }
-  return ss.str();
+    if (t == 0 || t == 1) {
+        ss << "00";
+    } else {
+        hours = (((t
+                      / 1000)
+                     / 60)
+                    / 60)
+            % 60;
+        if (hours < 10)
+            ss << "0" << hours;
+        else
+            ss << hours;
+    }
+    return ss.str();
 }
 template<Li::Literals::TimeValue T>
-std::string getFullTimeString(T const &t)
+std::string getFullTimeString(T const& t)
 {
-  return (
-        getHours(t)+":"+getMinutes(t)+":"+getSeconds(t)
-        );
+    return (
+        getHours(t) + ":" + getMinutes(t) + ":" + getSeconds(t));
 }
 }
 }
--- LiGi/timer.h
+++ LiGi/timer.h
@@ -18,191 +18,186 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#include <chrono>
-#include <thread>
 #include <atomic>
+#include <chrono>
+#include <concepts>
 #include <iostream>
 #include <mutex>
-#include <string>
 #include <sstream>
-#include <concepts>
+#include <string>
+#include <thread>
 
-namespace Li
-{
-namespace Literals
-{
+namespace Li {
+namespace Literals {
 
 template<class T>
-concept TimeValue  = requires(T a) {
-      std::is_integral<T>::value && std::is_arithmetic<T>::value && std::is_unsigned<T>::value;
-    };
+concept TimeValue = requires(T a)
+{
+    std::is_integral<T>::value&& std::is_arithmetic<T>::value&& std::is_unsigned<T>::value;
+};
 }
 
 using namespace std::literals;
 
 template<class T, Literals::TimeValue RValType = uint64_t>
-class Timer
-{
+class Timer {
 private:
-  T& u_ = static_cast<T&>(*this);
-  const T& u_c = static_cast<const T&>(*this);
-  /**
+    T& u_ = static_cast<T&>(*this);
+    const T& u_c = static_cast<const T&>(*this);
+    /**
    * @brief stopper atomic stopping variable to control flow
    */
-  std::atomic_bool &M_stopper;
-  /**
+    std::atomic_bool& m_stopper;
+    /**
    * @brief goal The time we actually want to reach!
    */
-  uint64_t M_goal;
+    uint64_t m_goal;
 
-  /**
+    /**
    * @brief delay Delay in which we actually increase variables
    */
-  uint64_t M_delay = 100;
+    uint64_t m_delay = 100;
 
-  /**
+    /**
    * @brief elapsed Stores, after delay and sleep, the already slept/waited time
    */
-  std::atomic_uint64_t M_timeLeft;
+    std::atomic_uint64_t m_timeLeft;
 
-  /**
+    /**
    * @brief sleep How long we sleep between checks
    */
-  uint64_t M_sleep = 20;
+    uint64_t m_sleep = 20;
 
 public:
+    operator RValType() const { return u_c.M_timeLeft; }
 
-  operator RValType() const {return u_c.M_timeLeft;}
-
-  void setTimeLeft(const Literals::TimeValue auto &set)
-  {
-    u_.M_timeLeft = set;
-  }
-
-  void setDelay(const Literals::TimeValue auto &set)
-  {
-    u_.M_delay = set;
-  }
-
-  void setSleep(const Literals::TimeValue auto &set)
-  {
-    u_.M_sleep = set;
-  }
-
-  void setGoal(Literals::TimeValue auto Goal)
-  {
-    u_.M_goal = Goal;
-  }
-
-  RValType getTimeLeft() const
-  {
-    uint64_t val = u_c.M_timeLeft;
-    return val;
-  }
-  RValType getGoal() const
-  {
-    return u_c.M_goal;
-  }
-
-  RValType getDelay() const
-  {
-    return u_c.M_delay;
-  }
-
-  RValType getSleep() const
-  {
-    return u_c.M_sleep;
-  }
-
-  std::atomic_bool &getStopper() const
-  {
-    return u_c.M_stopper;
-  }
-
-  void Pause()
-  {
-    u_.M_stopper = true;
-  }
-
-  void ToggleStopper()
-  {
-    u_.M_stopper = !u_.M_stopper;
-  }
-
-  void Unpause()
-  {
-    u_.M_stopper = false;
-  }
-
-  void ResetTime()
-  {
-    u_.M_timeLeft = u_c.M_goal;
-  }
-
-  void Stop()
-  {
-    u_.M_stopper = true;
-    u_.Pause();
-    u_.ResetTime();
-  }
-
-  void Resume()
-  {
-    u_.M_stopper = false;
-    // prevent deadlock - by decreasing `time_left` in RunTimer
-    //it's possible we overflow on the lower
-
-    // spectrum.
-    if(u_c.M_timeLeft <= 0 || u_c.M_timeLeft == UINT64_MAX)
+    void setTimeLeft(const Literals::TimeValue auto& set)
     {
-      return;
+        u_.m_timeLeft = set;
     }
-    u_.RunTimer();
-  }
 
-  void RunTimer()
-  {
-    auto t_start = std::chrono::steady_clock::now();
-    std::chrono::milliseconds t_delay(u_c.M_delay);
-
-    while(!u_c.M_stopper)
+    void setDelay(const Literals::TimeValue auto& set)
     {
-      std::this_thread::sleep_for(
-            std::chrono::milliseconds(u_c.M_sleep));
+        u_.m_delay = set;
+    }
 
-      auto t_now = std::chrono::steady_clock::now();
+    void setSleep(const Literals::TimeValue auto& set)
+    {
+        u_.m_sleep = set;
+    }
 
-      std::chrono::milliseconds t_elapsed =
-          std::chrono::duration_cast<std::chrono::milliseconds>(t_now - t_start);
-      if(t_delay <= t_elapsed)
-      {
-        t_start = t_now;
-        u_.M_timeLeft = u_.M_timeLeft - t_elapsed.count();
+    void setGoal(Literals::TimeValue auto Goal)
+    {
+        u_.m_goal = Goal;
+    }
 
-        if(u_c.M_timeLeft <= 0 || u_c.M_timeLeft == UINT64_MAX)
-        {
-          u_.M_timeLeft = 0; // possible overflow fixup
-          break; // we done
+    RValType getTimeLeft() const
+    {
+        uint64_t val = u_c.m_timeLeft;
+        return val;
+    }
+    RValType getGoal() const
+    {
+        return u_c.m_goal;
+    }
+
+    RValType getDelay() const
+    {
+        return u_c.m_delay;
+    }
+
+    RValType getSleep() const
+    {
+        return u_c.m_sleep;
+    }
+
+    std::atomic_bool& getStopper() const
+    {
+        return u_c.m_stopper;
+    }
+
+    void Pause()
+    {
+        u_.m_stopper = true;
+    }
+
+    void ToggleStopper()
+    {
+        u_.m_stopper = !u_.m_stopper;
+    }
+
+    void Unpause()
+    {
+        u_.m_stopper = false;
+    }
+
+    void ResetTime()
+    {
+        u_.m_timeLeft = u_c.m_goal;
+    }
+
+    void Stop()
+    {
+        u_.m_stopper = true;
+        u_.Pause();
+        u_.ResetTime();
+    }
+
+    void Resume()
+    {
+        u_.m_stopper = false;
+        // prevent deadlock - by decreasing `time_left` in RunTimer
+        //it's possible we overflow on the lower
+        // spectrum.
+        if (u_c.m_timeLeft <= 0 || u_c.m_timeLeft == UINT64_MAX) {
+            return;
         }
-      }
+        u_.RunTimer();
     }
-    return;
-  }
-  Timer(std::atomic_bool &stop, const Literals::TimeValue auto &Goal) :
-    M_stopper(stop), M_goal(Goal), M_timeLeft(Goal){}
 
-  virtual ~Timer() = default;
+    void RunTimer()
+    {
+        auto t_start = std::chrono::steady_clock::now();
+        std::chrono::milliseconds t_delay(u_c.m_delay);
 
-  // Copy Policy - disable
-  // It does no make sense at all to allow these. The timers have no single benefit of it
-  Timer &operator=(const Timer& rhs) = delete;
-  Timer (const Timer& src) = delete;
+        while (!u_c.m_stopper) {
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(u_c.m_sleep));
 
-  // Move Policy - disable
-  // again, no benefit
+            auto t_now = std::chrono::steady_clock::now();
 
-  Timer &operator=(Timer&& rhs) = delete;
-  Timer (const Timer&& src) = delete;
+            std::chrono::milliseconds t_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t_now - t_start);
+            if (t_delay <= t_elapsed) {
+                t_start = t_now;
+                u_.m_timeLeft = u_.m_timeLeft - t_elapsed.count();
+
+                if (u_c.m_timeLeft <= 0 || u_c.m_timeLeft == UINT64_MAX) {
+                    u_.m_timeLeft = 0; // possible overflow fixup
+                    break;             // we done
+                }
+            }
+        }
+        return;
+    }
+    Timer(std::atomic_bool& stop, const Literals::TimeValue auto& Goal)
+        : m_stopper(stop)
+        , m_goal(Goal)
+        , m_timeLeft(Goal)
+    {
+    }
+
+    virtual ~Timer() = default;
+
+    // Copy Policy - disable
+    // It does no make sense at all to allow these. The timers have no single benefit of it
+    Timer& operator=(const Timer& rhs) = delete;
+    Timer(const Timer& src) = delete;
+
+    // Move Policy - disable
+    // again, no benefit
+
+    Timer& operator=(Timer&& rhs) = delete;
+    Timer(const Timer&& src) = delete;
 };
 }
 #endif // TIMER_H
