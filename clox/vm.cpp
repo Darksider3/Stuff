66e3de066d0bfa2638df53d086c220ed0bd72daf
Darksider3 <github@darksider3.de>
2020-10-26 18:06:32 +0100

    clox: CPP vm class sceleton

--- clox/vm.cpp
+++ clox/vm.cpp
@@ -3,6 +3,40 @@
 #include "debug.h"
 #include <cstdio>
 
+class VirtualMachine {
+private:
+    Chunk *chunk;
+    uint8_t *ip; // instruction pointer, we could also call it PC for program counter
+    Value stack[STACK_MAX];
+    Value *stackTop;
+
+public:
+    explicit VirtualMachine(Chunk *chunk)
+    {
+        this->reset();
+        this->chunk = chunk;
+    }
+    
+    void reset()
+    {
+        stackTop = stack;
+    }
+    
+    void push(Value val)
+    {
+        *this->stackTop = val;
+        ++this->stackTop;
+    }
+    
+    Value pop()
+    {
+        --this->stackTop;
+        return *stackTop;
+    }
+    
+    InterpretResult run();
+    
+};
 // for convenience, define it here.
 // we'll have to pass it around the whole file probably
 // and it would be very abyssimal to do so manually
@@ -13,89 +47,95 @@ VM vm;
 
 static void resetStack()
 {
-  vm.stackTop = vm.stack;
+    vm.stackTop = vm.stack;
 }
 
 void push(Value value)
 {
-  *vm.stackTop = value; // store value on stackTop..
-  vm.stackTop++; // and advance it
+    *vm.stackTop = value; // store value on stackTop..
+    vm.stackTop++;        // and advance it
 }
 
 Value pop()
 {
-  vm.stackTop--;
-  return *vm.stackTop;
+    vm.stackTop--;
+    return *vm.stackTop;
 }
 void initVM()
 {
-  resetStack();
+    resetStack();
 }
 
 static InterpretResult run()
 {
-  
+
 #define READ_BYTE() (*vm.ip++) // increment IP and get it
 
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()]) // read a following constants value
 
-#define BINARY_OP(op)                                                  \
-do {                                                                   \
-  double b = pop();                                                    \
-  double a = pop();                                                    \
-  push(a op b);                                                        \
-} while(false)
+#define NEGATE_PUSH_POP() (push(-pop()))
+#define BINARY_OP(op)     \
+    do {                  \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
+    } while (false)
 
-  for (;;) //run until we dont want you to run anymore!
-  {
-    uint8_t instruction; // current instruction
+    for (;;) //run until we dont want you to run anymore!
+    {
+        uint8_t instruction; // current instruction
 #ifdef DEBUG_TRACE_EXECUTION
-    printf("         ");
-    for(Value *slot = vm.stack; slot < vm.stackTop; slot++)
-    {
-      printf("[ ");
-      printValue(*slot);
-      printf(" ]");
-    }
-    printf("\n");
-    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+        printf("         ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
-    switch(instruction = READ_BYTE())
-    {
-      case OP_CONSTANT: {
-        Value constant = READ_CONSTANT();
-        push(constant);
-        break;
-      }
-      
-      // BINARY OPs:
-      case OP_ADD: BINARY_OP(+); break;
-      case OP_SUBTRACT: BINARY_OP(-); break;
-      case OP_MULTIPLY: BINARY_OP(*); break;
-      case OP_DIVIDE: BINARY_OP(/); break;
+        switch (instruction = READ_BYTE()) {
+        case OP_CONSTANT: {
+            Value constant = READ_CONSTANT();
+            push(constant);
+            break;
+        }
 
-      case OP_NEGATE: push(-pop()); break; //negate current value on top of the stack and push it directly back onto it
+        // BINARY OPs:
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
+
+        case OP_NEGATE: push(-pop()); break; //negate current value on top of the stack and push it directly back onto it
 
       case OP_RETURN: {
-        printValue(pop());
-        printf("\n");
-        return INTERPRET_OK;
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
       }
+        }
     }
-  }
 #undef READ_CONSTANT
 #undef READ_BYTE
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(Chunk* chunk)
 {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
 }
 
 void freeVM()
 {
-
 }
