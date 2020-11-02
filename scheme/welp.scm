6bbc58b4e6fbeebb74502bde23f6b7957d73054c
Darksider3 <github@darksider3.de>
2020-11-02 20:21:20 +0100

    Update... i guess? Test cases!

--- scheme/welp.scm
+++ scheme/welp.scm
@@ -34,8 +34,6 @@
   )
 )
 
-(DBG_OUT (<= 1 1))
-
 (define (rdepth xtimes counter)
   (cond
     ((= xtimes counter) (println "done"))
@@ -47,21 +45,38 @@
         ((= x 0) 0)
         ((< x 0) (- x))))
 
-(define (rpow number original times)
+(define (RecursivePowDownCount number original times)
   (cond
-    ((<  times 0) (/ 1 (rpow-facade number (abs times))) ) ; handle negative exponent
+    ((<  times 0) (/ 1 (rpow number (abs times))) ) ; handle negative exponent
     ((=  times 0) (+ 1 0) ) ; handle x^0 case
-    ((=  times 1) (+ number 0) ) ; End of recursion - prevent off-by-one-error
-    (else (rpow (+ number number) original (- times 1)))
+    ((=  times 1) (+ number)) ; End of recursion - prevent off-by-one-error
+    ((= number 1) (+ 1))
+    (else (RecursivePowDownCount (* number original) original (- times 1)))
   )
 )
 ;  1      2      3           4            5            6              7
 ; 2+2 => 4+4 => 8+8(16) => 16+16(32) => 32+32(64) => 64+64(128) => 128+128 => 
-(define (rpow-facade number exponent)
+(define (rpow number exponent)
+  (RecursivePowDownCount number number exponent)
+)
+
+(define (pot x)
   (
-    rpow number number exponent
+    rpow x 2
   )
 )
 
-(DBG_OUT (rpow-facade 2 -5))
+(display "6^123      => ")
+(DBG_OUT (rpow 6 123))
+(display "2^6        => ")
+(DBG_OUT (rpow 2 6))
+(display "x^2(10^2)  => ")
+(DBG_OUT (pot 10))
+(display "2^-2       => ")
+(DBG_OUT (rpow 2 -2))
+(display "1^1        => ")
+(DBG_OUT (rpow 1 1))
+(display "2^0        => ")
+(DBG_OUT (rpow 2 0))
+
 ;(DBG_OUT (rfactor 2 2 6 0))
