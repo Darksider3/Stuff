e0e9d15b819dfacdfdcd94443131533fa772b163
Darksider3 <github@darksider3.de>
2020-11-11 17:04:06 +0100

    Excercises: Insertion-Sort shall only sort lists with >2 elements
    Also bring the old CMakeLists up to what it should be!

--- Excercises/CLRS/insertion_sort.cpp
+++ Excercises/CLRS/insertion_sort.cpp
@@ -1,63 +1,57 @@
 #include <iostream>
 
 template<typename SortT, typename LenT = size_t>
-SortT *insertion_sort(SortT *list, LenT size)
+SortT* insertion_sort(SortT* list, LenT size)
 {
-	if(size < 2)
-	{
-		return list;
-	}
+    if (size < 2) {
+        return list;
+    }
 
-	for(LenT j = 2; j < size; ++j)
-	{
-		SortT keyobj = list[j];
+    for (LenT j = 2; j < size; ++j) {
+        SortT keyobj = list[j];
 
-		LenT i = j - 1;
-		while(i > 0 && list[i] > keyobj)
-		{
-			list[i+1] = list[i];
-			i = i - 1;
-		}
-		list[i + 1] = keyobj;
-	}
+        LenT i = j - 1;
+        while (i > 0 && list[i] > keyobj) {
+            list[i + 1] = list[i];
+            i = i - 1;
+        }
+        list[i + 1] = keyobj;
+    }
 
-	return list;
+    return list;
 }
 
-int *insertion_sort(int* list, size_t len)
+int* insertion_sort(int* list, size_t len)
 {
-	if(len < 2)
-		return list;
+    if (len < 2)
+        return list;
 
-	for(size_t j = 2; j < len; ++j)
-	{
-		int key = list[j];
-		// insert list[j] into the sorted sequence A[1...j-1].
-		size_t i = j - 1;
-		while(i > 0 && list[i] > key)
-		{
-			list[i+1] = list[i];
-			i = i - 1;
-		}
-		list[i + 1] = key;
-	}
+    for (size_t j = 2; j < len; ++j) {
+        int key = list[j];
+        // insert list[j] into the sorted sequence A[1...j-1].
+        size_t i = j - 1;
+        while (i > 0 && list[i] > key) {
+            list[i + 1] = list[i];
+            i = i - 1;
+        }
+        list[i + 1] = key;
+    }
 
-	return list;
+    return list;
 }
 
 int main(int argc, char** argv)
 {
-	int ab[] = {0,3,1,2};
-	size_t size = 4;
+    int ab[] = { 0, 3, 1, 2 };
+    size_t size = 4;
 
-	insertion_sort<int, size_t>(ab, size);
+    insertion_sort<int, size_t>(ab, size);
 
-	//insertion_sort(ab, size);
-	for(size_t i = 0; i < size; ++i)
-	{
-		std::cout << ab[i] << "\n";
-	}
+    //insertion_sort(ab, size);
+    for (size_t i = 0; i < size; ++i) {
+        std::cout << ab[i] << "\n";
+    }
 
-	std::cout << std::endl;
-	return 0;
+    std::cout << std::endl;
+    return 0;
 }
