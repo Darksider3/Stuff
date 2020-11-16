8d2d0c61e8eaafbea823f504f212d01882964bbd
Darksider3 <github@darksider3.de>
2020-11-16 15:59:16 +0100

    Use match()-Function provided to handle things! :o

--- warc-parse/parser.cpp
+++ warc-parse/parser.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 	}
 
 	std::cout << "Processing file '" << argv[1] << "'. Be patient. :^)\n\n";
-	std::cout << "TRIM: " << FieldTrim<std::string>("    Some   .  ")
+	std::cout << "TRIM: " << Warc::detail::FieldTrim<std::string>("    Some   .  ")
 			  << "\n";
 	std::unique_ptr<std::ifstream> file = std::make_unique<std::ifstream>(std::string(argv[1]));
 	std::istream* is = file.get();
@@ -23,7 +23,7 @@ int main(int argc, char** argv)
 	}
 
 	std::string ver {};
-	Field_Map MAPP;
+	Warc::Field_Map MAPP;
 
 	/*ReadVersion(*is, ver);
 	ReadSubsequentFields(*is, MAPP);
@@ -38,24 +38,33 @@ int main(int argc, char** argv)
 	for_each(MAPP, DisplayKV);
 	*/
 
-	while (is->good()) {
-		auto Thus = ReadRecords(*is);
+	/*while (is->good()) {
+		auto Thus = Warc::ReadRecords(*is);
 		if (!holds(Thus)) {
 			std::cout << "No valid record.."
 					  << "\n";
 			continue;
 		}
 
-		std::string content = std::get<WarcRecord>(Thus).content();
+		std::string content = std::get<Warc::Record>(Thus).content();
 		std::cout << std::string(2, '\n')
 				  << std::string(20, '=')
+				  << "\nIs valid? " << std::boolalpha << std::get<Warc::Record>(Thus).validType()
 				  << "\nContent-Len: " << content.length() << "\n"
 				  << std::string(20, '=') << std::string(2, '\n')
 				  << content;
 		std::cout << "\n";
+	}*/
+
+	size_t total = 0;
+	while (is->good()) {
+		total += Warc::match(
+			Warc::ReadRecords(*is),
+			[](Warc::Record const& rec) -> size_t { return rec.content_length(); },
+			[](Warc::Error const&) -> size_t { std::cout << "Err!"; return 0; });
 	}
 
-	std::cout << std::endl;
+	std::cout << total << std::endl;
 
 	return 0;
 }
