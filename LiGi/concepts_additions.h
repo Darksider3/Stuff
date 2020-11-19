c712c6c21af92677b33f39d7adf33d7af4713879
Darksider3 <github@darksider3.de>
2020-11-19 09:35:22 +0100

    LiGi: concepts_additions containing my first two semi-usefull tasks!
    :)

---
+++ LiGi/concepts_additions.h
@@ -0,0 +1,13 @@
+#ifndef CONCEPTS_ADDITIONS_H
+#define CONCEPTS_ADDITIONS_H
+#pragma once
+
+#include <concepts>
+#include <unordered_map>
+
+template<typename T>
+concept EqualKeyValue = std::same_as<T, std::unordered_map<typename T::key_type, typename T::mapped_type, typename T::hasher, typename T::key_equal, typename T::allocator_type>>&& std::same_as<typename T::value_type::second_type, typename T::key_type>;
+template<typename T>
+concept NotEqualKeyValue = std::same_as<T, std::unordered_map<typename T::key_type, typename T::mapped_type, typename T::hasher, typename T::key_equal, typename T::allocator_type>> && !std::same_as<typename T::value_type::second_type, typename T::key_type>;
+
+#endif // CONCEPTS_ADDITIONS_H
---
+++ LiGi/test/concepts_additions.cpp
@@ -0,0 +1,38 @@
+#include "../concepts_additions.h"
+#include <iostream>
+
+void test(EqualKeyValue auto const& s)
+{
+	for (auto&& buck : s) {
+		std::cout << "EqualKeyBuck -> Key: '" << buck.first << "', second: " << buck.second << "\n";
+	}
+	return;
+}
+
+template<NotEqualKeyValue T>
+void test(T const& s)
+{
+	for (auto&& buck : s) {
+		std::cout << "NotEqualBuck -> Key: '" << buck.first << "', second: " << buck.second << "\n";
+	}
+	return;
+}
+
+int main(int argc, char** argv)
+{
+	std::unordered_map<std::string, std::string> eqT {};
+	eqT["first"] = "first";
+	eqT["sec"] = "sec";
+	eqT["3rd"] = "3rd";
+	eqT["4th"] = "4th";
+	std::unordered_map<int, std::string> diffT {};
+	diffT[0] = "first";
+	diffT[1] = "sec";
+	diffT[2] = "3rd";
+	diffT[3] = "4th";
+
+	test(eqT);
+	test(diffT);
+
+	return 0;
+}
