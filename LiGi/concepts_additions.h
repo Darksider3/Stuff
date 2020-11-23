7986a04d9f385379f6812f685e7c3bd9ba863a79
Darksider3 <github@darksider3.de>
2020-11-23 19:23:03 +0100

    LiGi:concepts_additions -> Adding namespace

--- LiGi/concepts_additions.h
+++ LiGi/concepts_additions.h
@@ -4,10 +4,10 @@
 
 #include <concepts>
 #include <unordered_map>
-
+namespace concepts {
 template<typename T>
 concept EqualKeyValue = std::same_as<T, std::unordered_map<typename T::key_type, typename T::mapped_type, typename T::hasher, typename T::key_equal, typename T::allocator_type>>&& std::same_as<typename T::value_type::second_type, typename T::key_type>;
 template<typename T>
 concept NotEqualKeyValue = std::same_as<T, std::unordered_map<typename T::key_type, typename T::mapped_type, typename T::hasher, typename T::key_equal, typename T::allocator_type>> && !std::same_as<typename T::value_type::second_type, typename T::key_type>;
-
+}
 #endif // CONCEPTS_ADDITIONS_H
