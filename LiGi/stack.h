1b7e95ed273bd81aa1ea22ae2c34134c4ac1221b
Darksider3 <github@darksider3.de>
2020-11-24 14:59:41 +0100

    Stack reformat

--- LiGi/stack.h
+++ LiGi/stack.h
@@ -4,62 +4,65 @@
 
 namespace Li {
 
-struct StackEntry : LLNode<StackEntry>{};
+struct StackEntry : LLNode<StackEntry> {
+};
 
 template<typename T>
-class Stack : private LinkedList<T>
-{
+class Stack : private LinkedList<T> {
 public:
-  bool empty();
-  T* top();
-  const T* top() const;
-  void pushd(T* Node);
-  void pushd(std::unique_ptr<T>);
-  void popd();
-  T *pop();
-  size_t height(){return this->size();}
+	bool empty();
+	T* top();
+	const T* top() const;
+	void pushd(T* Node);
+	std::unique_ptr<T> pushd(std::unique_ptr<T>);
+	void popd();
+	T* pop();
+	size_t height() { return this->size(); }
 };
+
 template<typename T>
 bool Stack<T>::empty()
 {
-  return (this->head() == nullptr);
+	return (this->head() == nullptr);
 }
 
 template<typename T>
 T* Stack<T>::top()
 {
-  return this->tail();
+	return this->tail();
 }
 
 template<typename T>
 const T* Stack<T>::top() const
 {
-  return this->tail();
+	return this->tail();
 }
 
 template<typename T>
 void Stack<T>::pushd(T* Node)
 {
-  this->append(Node);
-}
-template<typename T>
-void Stack<T>::pushd(std::unique_ptr<T> Node)
-{
-  this->pushd(Node.get());
+	this->append(Node);
 }
 
 template<typename T>
-T *Stack<T>::pop()
+std::unique_ptr<T> Stack<T>::pushd(std::unique_ptr<T> Node)
 {
-  T *ret = this->top;
-  this->popd();
-  return ret;
+	this->pushd(Node.get());
+	return Node;
+}
+
+template<typename T>
+T* Stack<T>::pop()
+{
+	T* ret = this->top;
+	this->popd();
+	return ret;
 }
 
 template<typename T>
 void Stack<T>::popd()
 {
-  this->remove(this->tail());
+	this->remove(this->tail());
 }
 
 }
