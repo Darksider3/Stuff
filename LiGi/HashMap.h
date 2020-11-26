876aa61653347c6a180127f1b5ba32f218e82fd9
Darksider3 <github@darksider3.de>
2020-11-26 17:06:06 +0100

    LiGi-HashMap: Search&Insert!

--- LiGi/HashMap.h
+++ LiGi/HashMap.h
@@ -1,5 +1,6 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
+#include <cmath>
 #include <cstdint>
 #include <memory>
 #include <vector>
@@ -7,11 +8,31 @@
 #include <iostream>
 
 namespace Li {
+
+#include <concepts>
+
 constexpr size_t map_initial_size = 53;
+constexpr size_t hsmPrime_1 = 463;
+constexpr size_t hsmPrime_2 = 283;
 using Size = size_t;
-template<typename Key, typename Value>
+
+template<typename T>
+concept Lengthy = requires(T a)
+{
+	std::is_default_constructible_v<T>;
+
+	{
+		a.length()
+	}
+	->std::convertible_to<Size>;
+
+	{
+		a == a
+	}
+	->std::same_as<bool>;
+};
+template<Lengthy Key, typename Value>
 class HashMap {
-public:
 public:
 	struct bucket;
 	struct hash_table;
@@ -25,9 +46,46 @@ public:
 	using MapVec = std::vector<BuckPtr>;
 	using TablePtr = ptr<hash_table>;
 
+public:
+	void insert(Key key, Value val)
+	{
+		BuckPtr item = make_bucket(key, val);
+		Size index = get_hash(item->s_key, m_table->size, 0);
+		bucket* cur = m_table->items.at(index).get();
+		for (Size i = 0; cur->s_key != Key {} && cur->s_value != Value {}; i++) {
+			index = get_hash(item->s_key, m_table->size, i);
+			cur = m_table->items.at(index).get();
+			std::cout << "Saw index '" << index << "'. \n";
+		}
+
+		m_table->items[index] = std::move(item);
+		m_table->count++;
+	}
+	Value search(Key key)
+	{
+		Size index = get_hash(key, m_table->size, 0);
+		BuckPtr old;
+		bucket* item = m_table->items[index].get();
+		for (Size i = 0; item->s_key != Key {} && item->s_value != Value {}; ++i) {
+			if (item->s_key == key) {
+				Value v = item->s_value;
+				//m_table->items[index] = std::move(item);
+				return v;
+			}
+
+			//m_table->items[index] = std::move(item);
+			index = get_hash(key, m_table->size, i);
+			item = m_table->items[index].get();
+		}
+
+		return nullptr;
+	}
+	void del();
+
+public:
 	struct bucket {
-		Value s_key;
-		Key s_value;
+		Key s_key;
+		Value s_value;
 	};
 
 	static BuckPtr make_bucket(const Key k, const Value v)
@@ -43,22 +101,46 @@ public:
 		MapVec items;
 	};
 
-	static TablePtr make_table()
+	static TablePtr make_table(Size MapSize = map_initial_size)
 	{
 		TablePtr Table = std::make_unique<hash_table>();
-		Table->items.resize(map_initial_size);
-		for (Size i = 0; i < map_initial_size; ++i) {
+		Table->items.resize(MapSize);
+		for (Size i = 0; i < MapSize; ++i) {
 			Table->items[i] = (make_bucket(Key {}, Value {}));
 		}
-		Table->size = 0;
+		Table->size = 53;
 		Table->count = 0;
 		return Table;
 	};
+
+	static Size m_hash(Lengthy auto&& var, Size prime, Size modulo)
+	{
+		Size _hash = 0;
+		const Size len = var.length();
+		for (Size i = 0; i < len; ++i) {
+			_hash += static_cast<Size>(std::pow(prime, len - (i + 1))) * static_cast<Size>(var[i]);
+			_hash = static_cast<Size>(_hash % modulo);
+		}
+		return _hash;
+	}
+
+	static Size get_hash(Lengthy auto&& var, const Size num_bucks, const Size attempts)
+	{
+		const Size first = m_hash(var, hsmPrime_1, num_bucks);
+		const Size second = m_hash(var, hsmPrime_2, num_bucks);
+		return (first + (attempts * (second + 1))) % num_bucks;
+	}
 
 	void removeBucket(Size x)
 	{
 		if (m_table->items.at(x))
 			m_table->items.at(x).release();
+	}
+
+	void removeTable(TablePtr& t)
+	{
+		if (t)
+			t.release();
 	}
 
 	TablePtr m_table;
@@ -70,10 +152,14 @@ public:
 
 int main()
 {
-	auto tab = Li::HashMap<char, char>();
-	tab.m_table = Li::HashMap<char, char>::make_table();
+	using bla = Li::HashMap<std::string, std::string>;
+	auto tab = Li::HashMap<std::string, std::string>();
+	tab.m_table = Li::HashMap<std::string, std::string>::make_table();
 
-	std::cout << tab.m_table->items.size();
+	tab.insert("brot", "world");
+	tab.insert("brigitte", "welt");
+
+	std::cout << tab.m_table->items.size() << "brot: " << tab.search("brot");
 	std::cout << std::endl;
 
 	//tab.removeBucket(3);
@@ -93,5 +179,12 @@ int main()
 		std::cout << "Key: " << b
 				  << "\nValue: " << a << "\n";
 	}
+
+	tab.removeTable(tab.m_table);
+	std::cout << "Hash: " << bla::get_hash(std::string("Hello world"), Li::hsmPrime_1, 120) << "\n";
+	if (tab.m_table)
+		std::cout << "still there that table \n";
+	else
+		std::cout << "table GONE. \n";
 }
 #endif
