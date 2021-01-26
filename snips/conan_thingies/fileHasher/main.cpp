99d883a954aee1ad3cf0ea7b4110d83719774780
Darksider3 <github@darksider3.de>
2021-01-26 13:58:17 +0100

    fileHasher: Building blocks for csv? Done

--- snips/conan_thingies/fileHasher/main.cpp
+++ snips/conan_thingies/fileHasher/main.cpp
@@ -521,6 +521,7 @@ private:
  */
 int main(int argc, char** argv)
 {
+    // disable(set to True) if we should really deallocate **everything** on exit.
     std::ios_base::sync_with_stdio(false);
     std::shared_ptr<DigestEncryptApp> pApp = std::make_shared<DigestEncryptApp>();
     try {
