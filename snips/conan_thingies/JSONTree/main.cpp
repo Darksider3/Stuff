29f730a766767c5eb9408725b62b97a179773602
Darksider3 <github@darksider3.de>
2021-02-26 12:16:34 +0100

    Sceleton for markdown link parser

--- snips/conan_thingies/JSONTree/main.cpp
+++ snips/conan_thingies/JSONTree/main.cpp
@@ -37,6 +37,63 @@ ListIndex getFilesListForIndex(fs::path& root, fs::path& Indexfile, std::string_
     return List;
 }
 
+/// \brief
+
+enum MKLINKSyms {
+    OP_START = 1,
+    FOUND_OPEN_BRACKET = 2,
+    FOUND_CLOSE_BRACKET = 3,
+    FOUND_OPTIONAL_CHAR = 4,
+    FOUND_OPEN_PARENTHESIS = 5,
+    FOUND_CLOSE_PARENTHESIS = 6,
+    OP_END = 7,
+    OP_NO_Link = 8
+};
+
+enum Certainity {
+    NoIdea = 1,
+    Possible = 2,
+    Probably = 3,
+    Definitly = 4
+};
+
+struct Symbol {
+    MKLINKSyms Sym;
+    char character;
+};
+
+Symbol start { .Sym = OP_START, .character = 0 };
+Symbol end { .Sym = OP_END, .character = 0 };
+Symbol open_bracket { .Sym = FOUND_OPEN_BRACKET, .character = '[' };
+Symbol close_bracket { .Sym = FOUND_CLOSE_BRACKET, .character = ']' };
+Symbol optional_char { .Sym = FOUND_OPTIONAL_CHAR, .character = 0 };
+Symbol open_parenth { .Sym = FOUND_OPEN_PARENTHESIS, .character = '(' };
+Symbol close_parenth { .Sym = FOUND_CLOSE_PARENTHESIS, .character = ')' };
+Symbol ERR { .Sym = OP_NO_Link, .character = 0 };
+
+class Parser {
+private:
+    void (*LookingForStarts)();
+    void (*LookingForEnds)();
+    void (*LookingForOpenBracket)();
+    void (*LookingForCloseBracket)();
+    void (*LookingForOptional)();
+    void (*LookingForOpenParenth)();
+    void (*LookingForCloseParenth)();
+    void (*ErrorOut)();
+
+    void (*functionarr[10])() = {
+        0,
+        LookingForStarts, LookingForOpenBracket, LookingForOpenBracket,
+        LookingForCloseBracket, LookingForOptional, LookingForOpenParenth,
+        LookingForCloseParenth, LookingForEnds,
+
+        ErrorOut
+    };
+};
+
+/// \return
+
 int main()
 {
     std::string_view FileSuffixFilter { ".md" };
