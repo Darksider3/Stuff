1b42ec6320bd9790682015833c37ecb505caa136
Darksider3 <github@darksider3.de>
2021-04-14 08:18:11 +0200

    Cookies handling improved

--- snips/Drogon/SetAndClearCookies/controllers/SetCookie.cc
+++ snips/Drogon/SetAndClearCookies/controllers/SetCookie.cc
@@ -6,8 +6,13 @@ void SetCookie::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<
     if (req->cookies().find(CookieName) != req->cookies().end()) {
         found = true;
     } else {
+        auto somedate = trantor::Date {};
+        somedate = somedate.after(60 * 60);
+        auto Cook = Cookie(CookieName, "Some content here i guess");
+        Cook.setExpiresDate(somedate);
         auto resp = HttpResponse::newRedirectionResponse("/setcookie");
         resp->addCookie(CookieName, "Some Content here i guess");
+        //resp->addCookie(Cook);
         callback(resp);
     }
 
@@ -15,6 +20,5 @@ void SetCookie::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<
     data.insert("title", "Set cookies!");
     data.insert("gotSet", found);
     auto resp = HttpResponse::newHttpViewResponse("setcookie.csp", data);
-    resp->removeCookie(CookieName);
     callback(resp);
 }
